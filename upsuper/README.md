d23ce797ed6a07a3a152370f3900aedfaa26215e
Xidorn Quan <quanxunzhen@gmail.com>
2014-07-19 13:51:42 +1000

    【新脚本】任意状态启用进度管理

--- upsuper/README.md
+++ upsuper/README.md
@@ -1,5 +1,11 @@
 # [upsuper](http://bangumi.tv/user/upsuper)
 
+## [任意状态启用进度管理](ep_status_enabler.user.js?raw=true)
+
+在 Bangumi 上，动画需要被标记为“在看”状态才能修改其进度。这个脚本使得动画无论处于什么收藏状态，都能够修改播放进度。未收藏时虽然也可以管理，但进度不会被显示出来。
+
+![Screenshot](images/screenshot_ep_status_enabler.png "Screenshot")
+
 ## [Bilibili 放送标志](bilibili_onair.user.js?raw=true)
 
 这个脚本会将首页上进度管理器中的绿标用于指示 Bilibili 上的新番更新情况。当B站将一个新番标记为今天内更新时，动画会出现绿标，同时对应的话数会被标绿，此外动画预览右上角的“放送中”标志变为一个链接，链接到B站上的最新话。
---
+++ upsuper/ep_status_enabler.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name        Bangumi 任意状态启用进度管理
+// @namespace   org.upsuper.bangumi
+// @include     http://bangumi.tv/subject/*
+// @include     http://bgm.tv/subject/*
+// @include     http://chii.in/subject/*
+// @exclude     http://bangumi.tv/subject/*/*
+// @exclude     http://bgm.tv/subject/*/*
+// @exclude     http://chii.in/subject/*/*
+// @version     1.4.1
+// ==/UserScript==
+
+function $(q) { return document.querySelector(q); }
+function $a(q) { return document.querySelectorAll(q); }
+if ($('.load-epinfo') && !$('.epStatusTool')) {
+  var toolWatched = '<div class="epStatusTool">' +
+    '<p id="epBtnCu_{0}" class="epBtnCu">看过</p>' +
+    '<a href="/subject/ep/{0}/status/queue?gh={1}"  ' +
+    'id="Queue_{0}" class="l ep_status">想看</a> ' +
+    '<a href="/subject/ep/{0}/status/drop?gh={1}"  ' +
+    'id="Drop_{0}" class="l ep_status">抛弃</a> ' +
+    '<a href="/subject/ep/{0}/status/remove?gh={1}"  ' +
+    'id="remove_{0}" class="l ep_status">撤消</a> </div>';
+  var toolNan = '<div class="epStatusTool">' +
+    '<p id="epBtnCu_{0}"></p>' +
+    '<a href="/subject/ep/{0}/status/watched?gh={1}" ' +
+    'id="Watched_{0}" class="l ep_status">看过</a> ' +
+    '<a href="/subject/ep/{0}/status/watched?gh={1}" ' +
+    'id="WatchedTill_{0}" class="l ep_status">看到</a> ' +
+    '<a href="/subject/ep/{0}/status/queue?gh={1}"  ' +
+    'id="Queue_{0}" class="l ep_status">想看</a> ' +
+    '<a href="/subject/ep/{0}/status/drop?gh={1}"  ' +
+    'id="Drop_{0}" class="l ep_status">抛弃</a> </div>';
+  var logout = $('a[href*="/logout/"]').href;
+  var gh = logout.substr(logout.indexOf('/logout/') + 8);
+  var $popups = $a('.prg_popup');
+  for (var i in $popups) {
+    var id = $popups[i].id.substr(8);
+    var $prg = $('#prg_' + id),
+    $prginfo = $('#prginfo_' + id);
+    if (!$prg || !$prginfo)
+      continue;
+    var tool = $prg.classList.contains('epBtnWatched') ?
+      toolWatched : toolNan;
+    tool = tool.replace(/\{0\}/g, id).replace(/\{1\}/g, gh);
+    $prginfo.innerHTML = tool + $prginfo.innerHTML;
+  }
+}
