492a36d4cfcd185a3a10e48be527fbd463b6f554
BinotaLiu <binota@binota.org>
2015-08-21 18:10:28 +0900

    [BIBE] Batch Add

--- binota/bangumi-index-batch-edit.user.js
+++ binota/bangumi-index-batch-edit.user.js
@@ -3,14 +3,17 @@
 // @namespace   BIBE
 // @description Batch edit comments in your index, and easily sort them by dragging.
 // @include     /^https?:\/\/((bgm|bangumi)\.tv|chii\.in)\/index\/\d+/
-// @version     0.0.2
+// @version     0.0.3
 // @grant       none
 // @require     https://code.jquery.com/ui/1.11.4/jquery-ui.min.js
 // ==/UserScript==
 
 //Check the owner of index, then insert the button for modify orders
-if($('.idBadgerNeue a.avatar').attr('href').search($('.grp_box a.avatar').attr('href')) >= 0)
-    $('.grp_box .tip_j').append(' / <a id="modifyOrder" class="chiiBtn" href="#">批量编辑</a>');
+if($('.idBadgerNeue a.avatar').attr('href').search($('.grp_box a.avatar').attr('href')) >= 0) {
+  $('.grp_box .tip_j').append(' / <a id="modifyOrder" class="chiiBtn" href="#">批量编辑</a>');
+  $('#indexCatBox ul').append('<li><a id="addRelateBatch" class="add thickbox" title="批量添加关联" href="#TB_inline?tb&height=500&width=420&inlineId=newIndexRelatedBatch"><span>+批量添加关联</span></a></li>');
+  $('<div style="display:none;" id="newIndexRelatedBatch"><div class="bibeBox" style="padding:10px"><label>输入条目URL或ID，如 http://bgm.tv/subject/265 或 265，一行一个</label><textarea rows="25" class="quick" name="urls"></textarea><input type="button" class="inputBtn" value="批量添加关联" name="submit" onclick="addRelateBatch()"></div></div>').insertBefore('#indexCatBox');
+}
 
 //Get formhash
 var formhash = $('input[name="formhash"]').val();
@@ -50,3 +53,14 @@ $('#modifyOrder').click(function() {
     $(this).html('保存完毕...！');
   });
 });
+
+window.addRelateBatch = function() {
+  $('.bibeBox input[name="submit"]').val('添加关联中...');
+  var url = $('#indexCatBox a')[0].href + '/add_related';
+  var items = $('.bibeBox textarea').val();
+  var items = items.split("\n");
+  for(i in items) {
+    $.post(url, {add_related: items[i].trim(), formhash: formhash, submit: '添加新关联'});
+  }
+  $('.bibeBox input[name="submit"]').val('添加完毕...！');
+}
