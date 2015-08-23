1f4407d2b95080aa13f5f1b42296d62e8ac523ac
BinotaLiu <binota@binota.org>
2015-08-23 09:09:45 +0900

    [BIAL] Update Regular Expression.

--- binota/bangumi-isbn-amazon-link.user.js
+++ binota/bangumi-isbn-amazon-link.user.js
@@ -2,9 +2,9 @@
 // @name        Bangumi-ISBN-Amazon-Link
 // @namespace   BIAL
 // @description Add amazon link by ISBN
-// @include     /https?:\/\/(bgm|bangumi|chii)\.(tv|in)\/subject\/\d+/
-// @version     0.0.1
+// @include     /https?:\/\/(bgm|bangumi|chii)\.(tv|in)\/subject\/\d+$/
+// @version     0.0.2
 // @grant       none
 // ==/UserScript==
 
-$('#infobox').html($('#infobox').html().replace(/ISBN: <\/span\>(\d+)/, 'ISBN: </span>$1 <a class="l" href="https://www.amazon.co.jp/dp/$1" target="_blank">日本Amazon</a>'));
+$('#infobox').html($('#infobox').html().replace(/ISBN: <\/span\>([\dX]+)/, 'ISBN: </span>$1 <a class="l" href="https://www.amazon.co.jp/dp/$1" target="_blank">日本Amazon</a>'));
