fc61891015c420439d39bc894a1b8a69fa43cc3e
BinotaLiu <binota@binota.org>
2015-08-24 22:34:15 +0900

    [BRRS] Offprint Mode.

--- binota/bangumi-rename-related-subjects.user.js
+++ binota/bangumi-rename-related-subjects.user.js
@@ -3,7 +3,7 @@
 // @namespace   BRRS
 // @description Quickly rename all related subjects at the same time.
 // @include     /^https?:\/\/((bgm|bangumi)\.tv|chii\.in)\/subject\/\d+\/add_related\/subject/
-// @version     0.0.6
+// @version     0.1.0
 // @grant       none
 // ==/UserScript==
 
@@ -23,10 +23,14 @@ var platforms = {
          {id: 3, name: '华语剧', meta: 'Television'},
          {id: 0, name: '其它', meta: 'Television'}]
 };
+
+var MODE_NORMAL = 0, MODE_OFFPRINT = 1;
+
 var subjects = [];
 var saving = 0;
 var editCount = 0;
-var editSummary = '标题修正+类型修正 by BRRS批量修改';
+var mode = MODE_NORMAL;
+var editSummary = '标题修正+类型修正 [BRRS]';
 var formhash = $('input[name="formhash"]').val();
 
 //Check platform type by url:
@@ -44,7 +48,8 @@ $('<div id="brrs-workspace" class="columns clearit" style="display: none; paddin
       '<input id="brrs-save" type="button" class="inputBtn" value="批量保存">' +
       '<small class="grey clearit rr">Powered by <a href="https://github.com/bangumi/scripts/tree/master/binota" target="_blank">BRRS</a>.</small>' +
   '</div>').insertBefore('.mainWrapper .columns');
-$('<a class="chiiBtn rr" id="brrs-launcher" href="#">BRRS 批量修改</a>').insertAfter('#modifyOrder');
+$('<a class="chiiBtn rr" id="brrs-launcher" href="#">BRRS</a>').insertAfter('#modifyOrder');
+$('<a class="chiiBtn rr" id="brrs-launcher-offprint" href="#">BRRS 单行本</a>').insertAfter('#modifyOrder');
 
 //Detect the type of platform:
 var platform = window.location.href.match(/add_related\/subject\/(anime|book|music|game|real)/)[1];
@@ -57,11 +62,44 @@ $('#crtRelateSubjects > li').each(function() {
   subjects.push(subject);
 });
 
+$('#brrs-launcher-offprint').click(function() {
+  mode = MODE_OFFPRINT;
+  subjects = [];
+  $('#crtRelateSubjects > li select').each(function() {
+    if($(this).val() != "1003") return;
+    
+    var subject = {};
+    subject.id = $(this).parent().parent().attr('item_id');
+    subject.title = $(this).parent().parent().find('.title a').html().trim();
+    subjects.push(subject);
+  });
+  $('#brrs-launcher').click();
+});
+
 $('#brrs-launcher').click(function() {
   $('#brrs-workspace').show();
+  if(mode == MODE_OFFPRINT) {
+    var li = '<tr>';
+    li += '<td></td>';
+    li += '<td><a class="chiiBtn" href="#" onclick="seriesTitle()">全部填入系列标题 + 序号</a></td>';
+    li += '<td>';
+    for(j in platforms[platform]) {
+      li += '<input class="platform radio" ' +
+                   'type="radio" ' +
+                   'value="' + platforms[platform][j].id + '" ' +
+                   'name="platform[0]"' +
+                   'id="brrs-platform-' + platforms[platform][j].id + '"' +
+                   'onchange="$(&quot;#brrs-subject-list .platform[value=\'' + platforms[platform][j].id + '\']&quot;).click()">';
+      li += '<label for="brrs-platform-' + platforms[platform][j].id + '">' + platforms[platform][j].name + '</label>';
+    }
+    li += '</td><td></td>';
+    li += '</tr>';
+    $('#brrs-subject-list').append(li);
+  }
   //Insert them into workspace:
   for(i in subjects) {
     var li = '<tr data-listid="' + i + '" data-edited="0">';
+    li += '<td><a class="chiiBtn" href="#" onclick="this.parentNode.parentNode.remove()">X</a></td>';
     li += '<td width="65%">';
     li += '<input class="inputtext renameSubject" '+
                  'name="brrs_titles[' + subjects[i].id + ']" ' +
@@ -120,6 +158,15 @@ window.saveTbSubject = function(i) {
   subjects[i].infobox = $('#TB_window textarea[name=infobox]').val();
   subjects[i].summary = $('#TB_window textarea[name=summary]').val();
   tb_remove();
+}
+
+window.seriesTitle = function() {
+  var title = $('.nameSingle a').html().trim();
+  var i = 1;
+  $('#brrs-subject-list input.inputtext').each(function() {
+    $(this).val(title + ' (' + (i++) + ')');
+  });
+  $('#brrs-subject-list tr[data-edited=0]').attr('data-edited', '1')
 }
 
 //Save:
