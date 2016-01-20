aa92c919969dd971f8d626c54b5b2a33fd55d557
chaucerling <chaucerling@gmail.com>
2016-01-20 18:04:00 +0800

    update to v0.2.0, support tracking real subjects

--- chaucerling/README.md
+++ chaucerling/README.md
@@ -4,5 +4,10 @@
 
 [讨论帖](https://bgm.tv/group/topic/317991)
 
-![Screenshot](http://r5.loli.io/yYJfi2.png)
-![Screenshot](http://r5.loli.io/mE3Ina.png)
+#### feature
+- extra 格子模式（目前只能使用平铺模式）：原本显示的格子在 `50`，脚本添加的格子在 `extra`。 如果不在首页点的格子或新增了在看条目，需要点击 `refresh` 按钮更新进度。
+- 平铺模式下能切换动画和三次元栏目 (fixed this [bug](https://github.com/bangumi/issues/issues/12))
+
+![Screenshot](http://r6.loli.io/NB3eEr.png)
+
+![Screenshot](http://r5.loli.io/2AB7j2.png)
--- chaucerling/bgm_tracking_improvement.user.js
+++ chaucerling/bgm_tracking_improvement.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bangumi tracking improvement
 // @namespace    BTI.chaucerling.bangumi
-// @version      0.1.2
+// @version      0.2.0
 // @description  tracking more than 50 subjects on bangumi index page
 // @author       chaucerling
 // @include      http://bangumi.tv/
@@ -17,13 +17,25 @@ var refresh = false;
 var extra_subjects = [];
 var subjects_size = 0;
 
-var cache_extra_subjects = (function() {
-  if (localStorage['extra_watching_animes'] === undefined){
+var cache_extra_subjects = function() {
+  if (localStorage['extra_subjects'] === undefined){
     return [];
   }
 
-  return JSON.parse(localStorage['extra_watching_animes']);
-})();
+  return JSON.parse(localStorage['extra_subjects']);
+}
+
+var cache_extra_animes_subjects = function() {
+  return cache_extra_subjects().filter(function(x) {
+  	return x.type === 2
+  })
+}
+
+var cache_extra_reals_subjects = function() {
+  return cache_extra_subjects().filter(function(x) {
+  	return x.type === 6
+  })
+}
 
 $("body").prepend("<style>\
 #ti-pages.categoryTab {\
@@ -62,8 +74,8 @@ function Subject() {
   this.prg_list_html = info.prg_list_html;
   this.prg_content_html = info.prg_content_html;
   this.thumb = info.thumb;
-  this.order = info.order;
   this.extra = info.extra; //boolean
+	this.type = info.type; // 2: amine, 6: real
 }
 
 function remove_img_src(str){
@@ -75,9 +87,9 @@ function get_display_subject_type() {
 }
 
 function change_dispaly(){
-  $('#prgCatrgoryFilter a[subject_type="0"],a[subject_type="6"]').hide();
-  $('#prgCatrgoryFilter a').removeClass('focus');
-  $('#prgCatrgoryFilter a[subject_type="2"]').addClass('focus');
+  // $('#prgCatrgoryFilter a[subject_type="0"],a[subject_type="6"]').hide();
+  // $('#prgCatrgoryFilter a').removeClass('focus');
+  // $('#prgCatrgoryFilter a[subject_type="2"]').addClass('focus');
 
   $('#switchNormalManager').removeClass();
   $('#switchNormalManager').hide();
@@ -93,71 +105,71 @@ function change_dispaly(){
 }
 
 function recover_dispaly(){
-  $('#prgCatrgoryFilter a').show();
+  // $('#prgCatrgoryFilter a').show();
   $('#switchNormalManager').show();
   $('.infoWrapper_tv div.infoWrapper').show();
   $('#ti-pages').hide();
 }
 
-// function get_all_display_subjects(subject_type) {
-//   switch(subject_type) {
-//     case "0": //all
-//       return 0;
-//     case "1": //book
-//       return 1;
-//     case "2": //anime
-//       return 2;
-//     case "6": //real
-//       return 6;
-//     default:
-//       return -1;
-//   }
-// }
-
-function get_watching_animes(){
-  // <a href="/anime/list/chaucerling/do" class="nav">在看</a>
-  var path = location.protocol+'//'+location.hostname + $("#navMenuNeue > li > ul > li > a.nav")[5].getAttribute('href');
-  var wathcing_list = [];
+function get_watching_animes_and_reals(){
+  var anime_list_path = location.protocol+'//'+location.hostname + $("#navMenuNeue > li:nth-child(1) > ul > li > a.nav")[5].getAttribute('href');
+	var real_list_path = location.protocol+'//'+location.hostname + $("#navMenuNeue > li:nth-child(5) > ul > li > a.nav")[5].getAttribute('href');
+  var wathcing_anime_list = [];
+	var wathcing_real_list = [];
   var page = 1;
+	var temp_page_list;
   extra_subjects = [];
-  for (page = 1;page < 5; page++){
+
+  for (page = 1;page < 5;page++){
     $.ajax({
       method: "GET",
-      accepts: "html",
-      dataType: "html",
-      url: path + "?page=" + page,
+      url: anime_list_path + "?page=" + page,
       async: false,
     }).success(function(data, textStatus, jqXHR) {
-      console.log(page)
-      var html = remove_img_src(data)
-      wathcing_list = $.merge(wathcing_list, $(html).find('#browserItemList > li'))
+      console.log(page);
+      var html = remove_img_src(data);
+			temp_page_list = $(html).find('#browserItemList > li');
+      wathcing_anime_list = $.merge(wathcing_anime_list, temp_page_list);
     });
-    if (wathcing_list.length !== 0 && wathcing_list.length % 24 !== 0){
-      break;
-    }
+    if (temp_page_list.length === 0) break;
   }
-  subjects_size = wathcing_list.length
-  if (subjects_size > 50) {
-    $('#ti-alert').text("Watching "+subjects_size+" animes, loading extra animes' progress (click to close).");
+	for (page = 1;page < 5;page++){
+    $.ajax({
+      method: "GET",
+      url: real_list_path + "?page=" + page,
+      async: false,
+    }).success(function(data, textStatus, jqXHR) {
+      console.log(page);
+      var html = remove_img_src(data);
+			temp_page_list = $(html).find('#browserItemList > li');
+      wathcing_real_list = $.merge(wathcing_real_list, temp_page_list);
+    });
+    if (temp_page_list.length < 24) break;
+  }
+
+  subjects_size = wathcing_anime_list.length + wathcing_real_list.length
+  if (wathcing_anime_list.length + wathcing_real_list.length > 50) {
+    $('#ti-alert').text("Watching "+wathcing_anime_list.length+" animes and "+wathcing_real_list.length+" reals, \
+			loading extra subjects' progress (click to close).");
     change_dispaly();
-    $.each(wathcing_list, function (index, value) {
-      get_subject_progress($(value).attr('id').split("_")[1], parseInt(index));
+    $.each(wathcing_anime_list.concat(wathcing_real_list), function (index, value) {
+      get_subject_progress($(value).attr('id').split("_")[1], (index - wathcing_anime_list.length < 0) ? 2 : 6);
     })
   } else {
     recover_dispaly();
     $('#ti-alert').show();
-    $('#ti-alert').text("Watching "+subjects_size+" animes (click to close).");
+    $('#ti-alert').text("Watching "+wathcing_anime_list.length+" animes and "+wathcing_real_list.length+" reals (click to close).");
   }
 }
 
-function get_subject_progress(subject_id, order){
+function get_subject_progress(subject_id, type){
   if (index_ids.includes(subject_id)) {
     check_get_all_subjects_finished();
     return;
   }
 
   $.get('/subject/' + subject_id, function(data){
-      var html = data;
+      var html = remove_img_src(data);
       var progress = $(html).find('#watchedeps').text().split("/")[0];
       var title = $(html).find('.nameSingle > a').text();
       var prg_list_html = $(html).find('.prg_list').addClass("clearit").html();
@@ -169,7 +181,7 @@ function get_subject_progress(subject_id, order){
                                  prg_list_html: prg_list_html,
                                  prg_content_html: prg_content_html,
                                  thumb: thumb,
-                                 order: order,
+                                 type: type,
                                  extra: true});
       extra_subjects.push(subject);
       check_get_all_subjects_finished();
@@ -184,13 +196,14 @@ function check_get_all_subjects_finished(){
   }
 
   this.counter = 0;
-  localStorage['extra_watching_animes'] = JSON.stringify(extra_subjects);
+	console.log(extra_subjects)
+  localStorage['extra_subjects'] = JSON.stringify(extra_subjects);
   add_extra_subjects(extra_subjects);
 }
 
 function add_extra_subjects(extra_subjects){
   for(i in extra_subjects){
-    create_subject_cell(extra_subjects[i], (i % 2) ? "event" : "odd");
+    create_subject_cell(extra_subjects[i]);
   }
 
   $('.infoWrapper_tv a.load-epinfo').cluetip({
@@ -210,23 +223,24 @@ function add_extra_subjects(extra_subjects){
 
   $('#subject_prg_content a.ep_status').click(function() {
     chiiLib.home.epStatusClick(this);
-		console.log('click');
     return false;
   });
 
   if (refresh === true) {
-    $('.infoWrapper_tv div.infoWrapper').hide();
-    $('.infoWrapper_tv div.infoWrapper.extra').show();
+    $('.infoWrapper_tv > div').hide();
+    $('.infoWrapper_tv > div.extra').show();
     $('#ti-pages a').removeClass('focus');
     $('#ti-pages a[data-page="extra"]').addClass('focus');
     $('#ti-pages a.refresh').removeClass('disabled');
     $('#ti-pages a.refresh').html('refresh');
   }
+
+	show_subjects($('#prgCatrgoryFilter a.focus').attr('subject_type'), $('#ti-pages a.focus').data('page') === 'extra');
 }
 
-function create_subject_cell(subject, odd){
+function create_subject_cell(subject){
   var extra = subject.extra ? " extra" : "";
-  $('.infoWrapper_tv').append("<div id='subjectPanel_"+subject.id+"' subject_type='2' class='"+odd+extra+" clearit infoWrapper tinyMode' \
+  $('.infoWrapper_tv').append("<div id='subjectPanel_"+subject.id+"' subject_type='"+subject.type+"' class='"+extra+" clearit infoWrapper tinyMode' \
     style='display:none;'>\
     <a href='/subject/"+subject.id+"' title='"+subject.title+"' class='grid tinyCover ll'>\
       <img src='"+subject.thumb+"' class='grid'>\
@@ -243,17 +257,68 @@ function create_subject_cell(subject, odd){
   $('#subject_prg_content').append(subject.prg_content_html);
 }
 
-function get_watching_animes_on_index(){
-  // 上限50是动画和三次元加起来的， 这里返回的会包含三次元的条目
+function show_subjects(subject_type, extra){
+	switch (subject_type) {
+		case "0": //all
+			if (extra){
+				$('.infoWrapper_tv > div').hide();
+				$('.infoWrapper_tv > div.extra').show();
+			} else {
+				$('.infoWrapper_tv > div').show();
+				$('.infoWrapper_tv > div.extra').hide();
+			}
+			break;
+    case "1": //book
+      break;
+    case "2": //anime
+			if (extra){
+				$('.infoWrapper_tv > div').hide();
+				$('.infoWrapper_tv > div[subject_type="2"].extra').show();
+			} else {
+				$('.infoWrapper_tv > div').hide();
+				$('.infoWrapper_tv > div[subject_type="2"]').show();
+				$('.infoWrapper_tv > div[subject_type="2"].extra').hide();
+			}
+			break;
+    case "6": //real
+			if (extra){
+				$('.infoWrapper_tv > div').hide();
+				$('.infoWrapper_tv > div[subject_type="6"].extra').show();
+			} else {
+				$('.infoWrapper_tv > div').hide();
+				$('.infoWrapper_tv > div[subject_type="6"]').show();
+				$('.infoWrapper_tv > div[subject_type="6"].extra').hide();
+			}
+		default:
+			break;
+	}
+	reset_odd_even();
+}
+
+function reset_odd_even(){
+	$.each($('.infoWrapper_tv > div:visible'), function(index, item) {
+		$(item).removeClass("even odd");
+		$(item).addClass((index % 2) ? "even" : "odd");
+	})
+}
+
+// 上限50是动画和三次元加起来的，这里返回的会包含三次元的条目
+function get_watching_animes_and_reals_on_index(){
   return $('.infoWrapper_tv > div');
 }
 
-function get_watching_animes_and_reals_on_index(){
+function get_watching_reals_on_index(){
+  return $('.infoWrapper_tv > div[subject_type="6"]');
+}
+
+function get_watching_animes_on_index(){
   return $('.infoWrapper_tv > div[subject_type="2"]');
 }
 
 // init
 $(document).ready(function(){
+	if (location.pathname !== "/") return;
+
   $("#cloumnSubjectInfo").prepend("<div id='ti-alert'\
   style='font-size: 14px;text-align: center;padding: 5px;background-color: #fcf8e3;display:none;'>\
   </div>");
@@ -263,30 +328,32 @@ $(document).ready(function(){
     <a type='button' data-page='refresh' class='refresh' href='javascript:void(0);'><span>refresh</span></a>\
     </div>");
 
-  var ary = get_watching_animes_and_reals_on_index();
-  var ary2 = get_watching_animes_on_index();
-  if (ary.length <= 49){
+	var ary1 = get_watching_animes_on_index();
+  var ary2 = get_watching_reals_on_index();
+  if (ary1.length + ary2.length <= 49){
     $('#ti-alert').show();
-    $('#ti-alert').text("Watching "+(ary2.length)+" animes. (click to close).");
-    localStorage.removeItem('extra_watching_animes');
+    $('#ti-alert').text("Watching "+ary1.length+" animes, "+ary2.length+" reals (click to close).");
+    localStorage.removeItem('extra_subjects');
     return;
   }
 
-  if (cache_extra_subjects.length > 0){
+  if (cache_extra_subjects().length > 0){
     $('#ti-alert').show();
-    $('#ti-alert').text("Maybe watching "+(ary2.length+cache_extra_subjects.length)+" animes, load form localStorage. (click to close).");
+    $('#ti-alert').text("Maybe watching "+(ary1.length+cache_extra_animes_subjects().length)+" animes, \
+			"+(ary2.length+cache_extra_reals_subjects().length)+" reals, \
+			load form localStorage. (click to close).");
     change_dispaly();
-    add_extra_subjects(cache_extra_subjects);
+    add_extra_subjects(cache_extra_subjects());
   } else {
     index_ids = [];
-    $.each(ary, function (index, value) {
+    $.each($.merge(ary1,ary2), function (index, value) {
       index_ids.push($(value).attr('id').split("_")[1]);
     });
     console.log(index_ids);
     $('#ti-alert').show();
-    $('#ti-alert').text("Maybe more than 50 watching animes, loading to comfirm. (click to close).");
+    $('#ti-alert').text("Maybe watching more than 50 animes and reals, loading to comfirm. (click to close).");
     setTimeout(function () {
-      get_watching_animes();
+      get_watching_animes_and_reals();
     }, 10);
   }
 });
@@ -296,25 +363,19 @@ $(document).on('click', '#ti-alert', function(e){
   $('#ti-alert').hide();
 });
 
-$('body').on('click', 'a.disabled', function(e){
-  event.preventDefault()
+$(document).on('click', 'a.disabled', function(e){
+  event.preventDefault();
 })
 
 $(document).on('click', '#ti-pages a', function(e){
   $('#ti-pages a').removeClass('focus');
-  if ($(this).data('page') == '50'){
+  if ($(this).data('page') !== 'refresh'){
     $(this).addClass('focus');
-    $('.infoWrapper_tv div.infoWrapper').show();
-    $('.infoWrapper_tv div.infoWrapper.extra').hide();
-  } else if ($(this).data('page') == 'extra') {
-    $(this).addClass('focus');
-    $('.infoWrapper_tv div.infoWrapper').hide();
-    $('.infoWrapper_tv div.infoWrapper.extra').show();
-  } else if ($(this).data('page') == 'refresh') {
-
+		show_subjects($('#prgCatrgoryFilter a.focus').attr('subject_type'), $(this).data('page') === 'extra');
+  } else {
     $(this).addClass('disabled');
     $(this).html('refreshing');
-    localStorage.removeItem('extra_watching_animes');
+    localStorage.removeItem('extra_subjects');
     $('.infoWrapper_tv div.extra').remove();
     var ary = get_watching_animes_and_reals_on_index();
     index_ids = [];
@@ -323,16 +384,21 @@ $(document).on('click', '#ti-pages a', function(e){
     });
     refresh = true
     setTimeout(function () {
-      get_watching_animes();
+      get_watching_animes_and_reals();
     }, 10);
   }
 })
 
+$('#prgCatrgoryFilter a').on('click', function(e){
+	show_subjects($(this).attr('subject_type'), $('#ti-pages a.focus').data('page') === 'extra')
+})
+
 // restore extra subjects' progress
 $(window).unload(function() {
-	if (cache_extra_subjects.length <= 0) return;
-	for (i in cache_extra_subjects){
-		cache_extra_subjects[i].prg_list_html = $("#subjectPanel_" + cache_extra_subjects[i].id + " .prg_list").html();
+	if (cache_extra_subjects().length <= 0) return;
+
+	for (i in cache_extra_subjects()){
+		cache_extra_subjects()[i].prg_list_html = $("#subjectPanel_" + cache_extra_subjects()[i].id + " .prg_list").html();
 	}
-	localStorage['extra_watching_animes'] = JSON.stringify(cache_extra_subjects);
+	localStorage['extra_subjects'] = JSON.stringify(cache_extra_subjects());
 })
