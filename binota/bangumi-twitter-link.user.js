ba98ee5ecdb955f48791a2714b2ec82b3ad43445
BinotaLiu <binota@binota.org>
2016-02-10 17:49:12 +0900

    [BTL] 適配完整鏈接的格式

--- binota/bangumi-twitter-link.user.js
+++ binota/bangumi-twitter-link.user.js
@@ -3,11 +3,10 @@
 // @namespace   BTL
 // @description Add twitter link in person page (According to Infobox).
 // @include     /https?:\/\/(bgm|bangumi|chii)\.(tv|in)\/person\/\d+$/
-// @version     0.0.1
+// @version     0.0.2
 // @grant       none
 // ==/UserScript==
 
 $('#infobox').html((function() {
-    return $('#infobox').html().replace(/([Tt]witter[^:]*): <\/span\>@?([\w\d_]+)/gi, '$1: </span><a class="l" href="https://twitter.com/$2" target="_blank">@$2</a>');
+    return $('#infobox').html().replace(/([Tt]witter[^:]*): <\/span\>@?(https?:\/\/twitter\.com\/|)([\w\d_]+)/gi, '$1: </span><a class="l" href="https://twitter.com/$3" target="_blank">@$3</a>');
 }));
-
