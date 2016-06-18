1671f46720b0568a5bd7efa9143beb3b609c0b24
prevailz <prevails.z@live.com>
2016-06-18 14:38:20 +0800

    引入jq 1.8. noConflict; 考虑性能, 渐变色模式色块宽度设为 85% 固定

--- prevails/ep_popu_visualizer.user.js
+++ prevails/ep_popu_visualizer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bangumi EpPopuVisualizer
 // @namespace    http://bgm.tv/user/prevails
-// @version      0.2.0
+// @version      0.2.1
 // @description  标注ep的讨论人气
 // @author       "Donuts."
 // @grant        GM_getValue
@@ -14,9 +14,12 @@
 // @match        http://bangumi.tv/
 // @match        http://chii.in/subject/*
 // @match        http://chii.in/
+// @require      https://code.jquery.com/jquery-1.8.min.js
 // @encoding     utf-8
 // ==/UserScript==
 
+
+var epv_jq = $.noConflict();
 
 (function () {
     'use strict';
@@ -26,20 +29,20 @@
     }
 
     function prgListExists() {
-        return $('ul.prg_list').length !== 0;
+        return epv_jq('ul.prg_list').length !== 0;
     }
 
     function getMax(arr) {
-        if (arr.length === 0) {
-            return 0;
-        }
-        return arr.reduce(function (a, b) {return a > b ? a : b;});// 不允许对空array使用reduce
+    	if (arr.length === 0) {
+    		return 0;
+    	}
+        return arr.reduce(function (a, b) {return a > b ? a : b;});
     }
 
     function getValues($lis) {
         var ids = [];
         $lis.each(function () {
-            var $a = $(this).find('a');
+            var $a = epv_jq(this).find('a');
             var id = $a[0].id.replace("prg_", '');
             ids.push(id);
         });
@@ -63,6 +66,7 @@
 
     function default_getWidth($a) {
         var width = getPixel($a, 'width');
+        console.log(width);
         return 6 + width;// padding + border == 6
     }
 
@@ -82,15 +86,15 @@
                 }
                 var colors = values.map(getColor(colorX, max));
                 $lis.each(function (index) {
-                    var $li = $(this);
-                    var width = default_getWidth($('a', $li));
-                    $li.prepend('<div style="height:3px;width:'+width+'px;background:' + colors[index] + ';"></div>');
+                    var $li = epv_jq(this);
+                    var width = default_getWidth(epv_jq('a', $li));
+                    $li.prepend('<div style="height:3px;width:85%;background:' + colors[index] + ';"></div>');
                 });
             };
             /////////////////////////////
             case 'histogram':
             var color = GM_getValue("histogram_color");
-            var $expA = $('ul.prg_list a.load-epinfo:eq(0)');
+            var $expA = epv_jq('ul.prg_list a.load-epinfo:eq(0)');
             var height = histogram_getHeight($expA);
             var bottomPx = $expA.css('margin-bottom');
             var rightPx = $expA.css('margin-right');
@@ -101,7 +105,7 @@
                 }
                 var lengths = values.map(getLength(height, max));
                 $lis.each(function (index) {
-                    var $li = $(this);
+                    var $li = epv_jq(this);
                     var html =
                         '<div style="' +
                         'position:absolute;' +
@@ -118,7 +122,7 @@
     }
 
     function getEpValue(id) {
-        var value = $("#subject_prg_content > #prginfo_" + id + " > span > span > small.na").html();
+        var value = epv_jq("#subject_prg_content > #prginfo_" + id + " > span > span > small.na").html();
         value = value.substring(2, value.length - 1);
         return parseInt(value);
     }
@@ -149,15 +153,18 @@
     }
 
     function addControlPanel() {
-        $('#columnHomeB').append('<div id="ep_popu_visualizer_control_panel" style="padding-left:10px;"><a class="l epv_control_panel_switch" href="javascript:;">EpPopuVisualizer 设置</a></div>');
-        var $cp = $('#ep_popu_visualizer_control_panel');
+        epv_jq('#columnHomeB').append('<div id="ep_popu_visualizer_control_panel" style="padding-left:10px;"></div>');
+
+        var $cp = epv_jq('#ep_popu_visualizer_control_panel');
+        $cp.append('<a class="l epv_control_panel_switch" href="javascript:;">EpPopuVisualizer 设置</a>');
         var mode = GM_getValue('viewMode');
         function refreshColor() {
-            $('#epv_color_pick input').val(GM_getValue(GM_getValue('viewMode') + '_color'));
+            epv_jq('#epv_color_pick input').val(GM_getValue(GM_getValue('viewMode') + '_color'));
         }
-        $('#ep_popu_visualizer_control_panel > a.epv_control_panel_switch').toggle(function () {
-            if ($(".epv_content", $cp).length === 0) {
-                var $content = $(
+        // toggle 传入函数的特性在1.9被移除, 因此 a 直接触发了hide()效果, 两个函数没有绑定.
+        epv_jq('#ep_popu_visualizer_control_panel > a.epv_control_panel_switch').toggle(function () {
+            if (epv_jq(".epv_content", $cp).length === 0) {
+                var $content = epv_jq(
                     '<div class="epv_content" style="margin-top:10px;display:none;">'+
                     '<div id="epv_mode_select">模式切换: <input type="radio" name="viewMode" value="default" '+(mode === 'default' ? 'checked' : '')+' />渐变色 (默认) <input type="radio" name="viewMode" value="histogram" '+(mode === 'histogram' ? 'checked' : '')+' />条形图 </div>'+
                     '<div id="epv_color_pick">颜色选择: <input type="text" id="epv_color_text_input"  value="'+GM_getValue(mode + '_color')+'"> <input id="epv_color_input" type="color" value="'+GM_getValue(mode + '_color')+'" style="height:1.3em;"></div>'+
@@ -165,25 +172,26 @@
                 
                 $cp.append($content);
 
-                $('#epv_mode_select input').click(function () {
-                    GM_setValue('viewMode', $(this).val());
+                epv_jq('#epv_mode_select input').click(function () {
+                    GM_setValue('viewMode', epv_jq(this).val());
                     refreshColor();
                 });
-                $('#epv_color_pick input').change(function () {
-                    GM_setValue(GM_getValue('viewMode') + "_color", $(this).val());
+                epv_jq('#epv_color_pick input').change(function () {
+                    GM_setValue(GM_getValue('viewMode') + "_color", epv_jq(this).val());
                     refreshColor();
                 });
 
             }
-            $(".epv_content", $cp).slideDown('fast');
+            epv_jq(".epv_content", $cp).slideDown('fast');
         },
         function () {
-            $(".epv_content", $cp).slideUp('fast');
+            epv_jq(".epv_content", $cp).slideUp('fast');
         });
     }
 
     function main() {
         init();
+        console.log(324);
         if (isRootpath()) {
             addControlPanel();
         }
@@ -192,13 +200,13 @@
         }
         var $uls;
         if (isRootpath()) {
-            $uls = $('div.infoWrapper_tv ul.prg_list');
+            $uls = epv_jq('div.infoWrapper_tv ul.prg_list');
         } else {
-            $uls = $('ul.prg_list');
+            $uls = epv_jq('ul.prg_list');
         }
         var show = getShowMethod(GM_getValue('viewMode'));
         $uls.each(function () {
-            var $lis = $('li:not(.subtitle)', this);
+            var $lis = epv_jq('li:not(.subtitle)', this);
             var values = getValues($lis);
             show($lis, values);
 
