3d5b3481b1dd9270828a78d15b517729ee90a9a7
Xidorn Quan <me@upsuper.org>
2016-08-06 01:15:22 +1000

    【标签管理】使用新特性重构脚本

--- upsuper/tag_batch_editor.user.js
+++ upsuper/tag_batch_editor.user.js
@@ -2,70 +2,94 @@
 // @name        Bangumi 标签批量管理
 // @namespace   org.upsuper.bangumi
 // @include     /^https?://(bgm\.tv|chii\.in|bangumi\.tv)/(anime|book|music|game|real)/list/.+$/
-// @version     2.4
+// @version     2.5
 // @grant       GM_addStyle
 // ==/UserScript==
 
-var TIMEOUT = 3000,
-    RETRY_INTERVAL = 1000;
+const TIMEOUT = 3000;
+const MAX_RETRIES = 3;
+const RETRY_INTERVAL = 1000;
+const ITEMS_PER_PAGE = 24;
 
-function $(q, e) { return (e ? e : document).querySelector(q); }
-function $a(q, e) { return (e ? e : document).querySelectorAll(q); }
-function $c(t) { return document.createElement(t); }
-String.prototype.u$format = function () {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function (match, i) {
-    return args[i] !== undefined ? args[i] : match;
+let $ = (q, e) => (e ? e : document).querySelector(q);
+let $a = (q, e) => (e ? e : document).querySelectorAll(q);
+let $c = t => document.createElement(t);
+
+function runAsync(iter) {
+  return new Promise((resolve, reject) => {
+    function iterate() {
+      let next = iter.next();
+      if (!next.done) {
+        next.value.then(iterate);
+      } else {
+        resolve(next.value);
+      }
+    }
+    iterate();
   });
-};
+}
 
 // check username
-var username = $('.idBadgerNeue a.avatar').href.split('/').pop();
-var urlPieces = location.href.split(/[\/\?]/);
-if (username !== urlPieces[5])
+let username = $('.idBadgerNeue a.avatar').href.split('/').pop();
+let urlPieces = location.href.split(/[\/\?]/);
+if (username !== urlPieces[5]) {
   return;
-var urlBase = urlPieces.slice(0, 7).join('/');
+}
+let urlBase = urlPieces.slice(0, 7).join('/');
 
 // insert style
-GM_addStyle('\
-#userTagList>li>a.l { margin-right: 32px; } \
-#userTagList>li>a.__u_edit, #userTagList>li>a.__u_del { \
-  width: 16px; height: 16px; \
-  padding: 4px 0; float: right; \
-  text-align: center; color: #aaa; \
-  line-height: 16px; font-size: 10px; \
-} \
-a.__u_add { \
-  width: 16px; height: 16px; \
-  padding: 3px 0; float: right; \
-  text-align: center; color: #fff; \
-  line-height: 16px; font-size: 10px; \
-} \
-#__u_pb { \
-  position: fixed; \
-  top: 0; \
-  width: 100%; \
-} \
-#__u_pb[max="0"] { \
-  display: none; \
-} \
-');
+GM_addStyle(`
+#userTagList>li>a.l {
+  margin-right: 32px;
+}
+#userTagList>li>a.__u_edit,
+#userTagList>li>a.__u_del {
+  width: 16px; height: 16px;
+  padding: 4px 0; float: right;
+  text-align: center; color: #aaa;
+  line-height: 16px; font-size: 10px;
+}
+a.__u_add {
+  width: 16px; height: 16px;
+  padding: 3px 0; float: right;
+  text-align: center; color: #fff;
+  line-height: 16px; font-size: 10px;
+}
+#__u_pb {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+#__u_pb[max="0"] {
+  display: none;
+}
+`);
 
 // add progress bar
-var $pb = $c('progress');
-var workingJobs = 0;
+let $pb = $c('progress');
 $pb.id = '__u_pb';
 $pb.max = $pb.value = 0;
 document.body.appendChild($pb);
 
-// update tag list
-var $tags = $a('#userTagList>li');
-for (var i = 0; i < $tags.length; i++) {
-  var $tag = $tags[i],
-  $anchor = $tag.getElementsByTagName('a')[0];
-  var $button;
+let workingJobs = 0;
+function increaseWorkingJobs(num) {
+  workingJobs++;
+  $pb.max += num;
+}
+function decreaseWorkingJobs() {
+  workingJobs--;
+  if (workingJobs == 0)
+    location.reload();
+}
+function progress() {
+  $pb.value++;
+}
 
-  $button = $c('a');
+// update tag list
+for (let $tag of $a('#userTagList>li')) {
+  $anchor = $tag.getElementsByTagName('a')[0];
+
+  let $button = $c('a');
   $button.href = '#';
   $button.className = '__u_del';
   $button.title = '删除';
@@ -81,197 +105,186 @@ for (var i = 0; i < $tags.length; i++) {
 }
 
 // add checkboxes to item list
-var $items = $a('#browserItemList>li');
-for (var i = 0; i < $items.length; i++) {
-  var $item = $items[i],
-  $modify = $('.collectModify', $item);
-
-  var $checkbox = $c('input');
+for (let $item of $a('#browserItemList>li')) {
+  let $modify = $('.collectModify', $item);
+  let $checkbox = $c('input');
   $checkbox.type = 'checkbox';
   $modify.insertBefore($checkbox, $modify.firstChild);
 }
 
 // add new tag button
-var $panel = $('#userTagList').parentNode;
-var $newtag = $c('a');
+let $panel = $('#userTagList').parentNode;
+let $newtag = $c('a');
 $newtag.href = '#';
 $newtag.className = '__u_add';
 $newtag.title = '添加';
 $newtag.textContent = '+';
-$newtag.addEventListener('click', function (evt) {
-  var ids = [];
+$newtag.addEventListener('click', evt => {
   evt.preventDefault();
-
-  var $items = $a('#browserItemList>li');
-  for (var i = 0; i < $items.length; i++) {
-    var $item = $items[i],
+  let ids = [];
+  for (let $item of $a('#browserItemList>li')) {
     $chk = $('input[type=checkbox]', $item);
-    if ($chk.checked)
+    if ($chk.checked) {
       ids.push($item.id.substr(5));
+    }
   }
-
   if (!ids.length) {
     alert('请先选择条目');
     return;
   }
-
-  var newTag = prompt('请输入新标签名：');
-  if (!newTag) return;
-  batchChangeTag(ids, null, newTag, function (id) {
-    //$('#item_{0} input[type=checkbox]'.u$format(id)).checked = false;
-  });
+  let newTag = prompt('请输入新标签名：');
+  if (newTag) {
+    increaseWorkingJobs(ids.length);
+    batchChangeTag(ids, null, newTag).then(decreaseWorkingJobs);
+  }
 });
 $panel.insertBefore($newtag, $panel.firstChild);
 
 // bind event
-$('#userTagList').addEventListener('click', function (evt) {
-  var className = evt.target.className;
-  if (className != '__u_edit' && className != '__u_del')
+$('#userTagList').addEventListener('click', evt => {
+  let className = evt.target.className;
+  if (className != '__u_edit' && className != '__u_del') {
     return;
-
-  var $li = evt.target.parentNode;
-  var oldTag = decodeURIComponent($('a.l', $li).href.split('=')[1]);
-  var newTag;
+  }
+  let $li = evt.target.parentNode;
+  let oldTag = decodeURIComponent($('a.l', $li).href.split('=')[1]);
+  let newTag;
   evt.preventDefault();
   if (className == '__u_del') {
-    if (!confirm('确认要删除标签“{0}”吗？'.u$format(oldTag)))
-    return;
+    if (!confirm(`确认要删除标签“${oldTag}”吗？`)) {
+      return;
+    }
     newTag = '';
   } else {
     newTag = prompt('请输入新的标签名：');
-    if (!newTag) return;
+    if (!newTag) {
+      return;
+    }
   }
-
   changeTagName(oldTag, newTag, $li);
 }, true);
 
 // process
 function changeTagName(oldTag, newTag, $li) {
-  var $anchor = $('a.l', $li);
-  var num = parseInt($('small', $anchor).textContent)
-    , pageNum = Math.ceil(num / 24);
-  var ids = [];
-  var url = urlBase + '?tag=' + encodeURIComponent(oldTag) + '&page=';
-  $pb.max += num + pageNum;
-  workingJobs++;
+  let $anchor = $('a.l', $li);
+  let num = parseInt($('small', $anchor).textContent);
+  let pageNum = Math.ceil(num / ITEMS_PER_PAGE);
+  let url = `${urlBase}?tag=${encodeURIComponent(oldTag)}&page=`;
+  increaseWorkingJobs(num + pageNum);
+  getIds(url, pageNum)
+    .then(ids => batchChangeTag(ids, oldTag, newTag))
+    .then(decreaseWorkingJobs);
+}
 
-  getListPage(1, function () {
-    batchChangeTag(ids, oldTag, newTag, updateProcessBar);
-  });
-
-  function updateProcessBar() {
-    $pb.value++;
-  }
-
-  function getListPage(page, callback) {
-    var xhr = new XMLHttpRequest();
-    var received = false;
-
-    var watchdog = setTimeout(function () {
+function fetchWithTimeout(url) {
+  return new Promise((resolve, reject) => {
+    let watchdog = setTimeout(() => {
       watchdog = 0;
-      if (received) return;
-      xhr.abort();
-      getListPage(page, callback);
+      reject("timeout");
     }, TIMEOUT);
-
-    xhr.open('GET', url + page, true);
-    xhr.send(null);
-    xhr.onreadystatechange = function () {
-      if (this.readyState != 4 || this.status != 200)
-        return;
-      received = true;
-      if (watchdog) {
-        clearTimeout(watchdog);
-        watchdog = 0;
+    fetch(url).then(resp => {
+      // We have timeouted, abandon this request.
+      // TODO We really want to abort the request earlier like what
+      //      we could do with XHR. But fetch doesn't have that yet.
+      if (!watchdog) {
+        throw "timeout";
       }
-
-      var content = this.responseText;
-      var regx = /<li id="item_(\d+)"/g, match;
-      while (match = regx.exec(content))
-        ids.push(match[1]);
-
-      updateProcessBar();
-      if (page < pageNum)
-        getListPage(page + 1, callback);
-      else
-        callback();
-    };
-  }
+      if (!resp.ok) {
+        throw `request failed with ${resp.status}`;
+      }
+      clearTimeout(watchdog);
+      return resp.text();
+    }).then(resolve).catch(reject);
+  });
 }
 
-function batchChangeTag(ids, oldTag, newTag, updateProcessBar) {
-  var $iframe = $c('iframe');
-  $iframe.style.display = 'none';
-  document.body.appendChild($iframe);
-
-  function nextItem() {
-    var id = ids.shift();
-    if (id) {
-      changeTag(id, oldTag, newTag,
-                $iframe, updateProcessBar, nextItem);
-    } else {
-      workingJobs--;
-      if (workingJobs == 0)
-        location.reload();
+function getIds(urlBase, pageNum) {
+  function* iterator() {
+    let ids = [];
+    for (let page = 1; page <= pageNum; page++) {
+      let url = `${urlBase}${page}`;
+      let done = false;
+      for (let tries = 0; !done && tries < MAX_RETRIES; tries++) {
+        yield fetchWithTimeout(url).then(content => {
+          let regex = /<li id="item_(\d+)"/g;
+          let match;
+          while (match = regex.exec(content)) {
+            ids.push(match[1]);
+          }
+          done = true;
+        }).catch(e => console.error(e));
+      }
+      if (!done) {
+        console.error(`exceeded max retry times for page ${page}`);
+      }
+      progress();
     }
+    return ids;
   }
-  nextItem();
+  return runAsync(iterator());
 }
 
-function changeTag(id, oldTag, newTag,
-                   $iframe, updateProcessBar, callback) {
-  var url = '/update/' + id;
-  var watchdog;
-  stage0();
-
-  function stage0() {
-    $iframe.src = url;
-    $iframe.onload = stage1;
-    finished = false;
-    watchdog = setTimeout(function () {
-      if ($iframe.onload != stage1)
-        return;
-      url += '?';
-      stage0();
-    }, TIMEOUT);
-  }
-  function stage1() {
-    if (watchdog) {
-      clearTimeout(watchdog);
-      watchdog = 0;
+function batchChangeTag(ids, oldTag, newTag) {
+  function* iterator() {
+    let $iframe = $c('iframe');
+    $iframe.style.display = 'none';
+    document.body.appendChild($iframe);
+    for (let id of ids) {
+      yield changeTag(id, oldTag, newTag, $iframe);
+      progress();
     }
-
-    var doc = $iframe.contentDocument;
-    var $tags = doc.getElementById('tags');
-    if (!$tag)
-      setTimeout(stage0, RETRY_INTERVAL);
-
-    if (oldTag) {
-      var tags = $tags.value.trim().split(/\s+/);
-      for (var i = 0; i < tags.length; i++)
-      if (tags[i].toLowerCase() == oldTag.toLowerCase())
-        tags[i] = newTag;
-      $tags.value = tags.join(' ');
-    } else {
-      $tags.value += ' ' + newTag;
-    }
-
-    doc.forms[0].submit();
-    $iframe.onload = stage2;
-    watchdog = setTimeout(function () {
-      if ($iframe.onload != stage2)
-        return;
-      doc.forms[0].submit();
-    }, TIMEOUT);
   }
-  function stage2() {
-    if (watchdog) {
-      clearTimeout(watchdog);
-      watchdog = 0;
-    }
+  return runAsync(iterator());
+}
 
-    $iframe.onload = undefined;
-    updateProcessBar(id);
-    callback();
+function changeTag(id, oldTag, newTag, $iframe) {
+  function waitForIframeLoad() {
+    return new Promise((resolve, reject) => {
+      let watchdog = setTimeout(() => {
+        $iframe.onload = undefined;
+        reject();
+      }, TIMEOUT);
+      $iframe.onload = () => {
+        clearTimeout(watchdog);
+        $iframe.onload = undefined;
+        resolve();
+      };
+    });
   }
+  let url = `/update/${id}`;
+  function* iterator() {
+    let done = false;
+    for (let tries = 0; !done && tries < MAX_RETRIES; tries++) {
+      yield Promise.resolve().then(() => {
+        $iframe.src = url;
+        return waitForIframeLoad();
+      }).then(() => {
+        let doc = $iframe.contentDocument;
+        let $tags = doc.getElementById('tags');
+        if (!$tags) {
+          throw "unexpected document";
+        }
+        if (oldTag) {
+          $tags.value = $tags.value.trim().split(/\s+/).map(value => {
+            if (value.toLowerCase() == oldTag.toLowerCase())
+              return newTag;
+            return value;
+          }).join(' ');
+        } else {
+          $tags.value += ' ' + newTag;
+        }
+        doc.forms[0].submit();
+        return waitForIframeLoad();
+      }).then(() => {
+        done = true;
+      }).catch(e => {
+        console.log(e);
+        url += '?';
+      });
+    }
+    if (!done) {
+      throw `exceeded max retry times for item ${id}`;
+    }
+  }
+  return runAsync(iterator());
 }
