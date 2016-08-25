765e67f9840c59db62d8789206ac90e525c06559
protodonuts <prevails.z@live.com>
2016-08-25 15:44:39 +0800

    bug fix

--- prevails/set_privacy_on_drop.user.js
+++ prevails/set_privacy_on_drop.user.js
@@ -3,7 +3,7 @@
 // @namespace    bangumi.scripts.prevails.setprivacyondrop
 // @author       "Donuts."
 // @description  在所有你主动点"抛弃"的时刻, 自动勾选"仅自己可见".
-// @version      1
+// @version      1.0.1
 // @include      /^https?:\/\/(bgm\.tv|bangumi\.tv|chii\.in)\/(update|subject)\/\d+(\?.*)?$/
 // @grant        none
 // @encoding     utf-8
@@ -18,22 +18,12 @@ function setPrivacy(val) {
     prv.checked = val;
 }
 
-[
-    document.getElementById('dropped'),
-    document.querySelector('#SecTab [onclick*=dropped]'),
-].forEach(i => {
-    if (i) {
-        i.addEventListener('click', () => setPrivacy(true));
-    }
-});
+document.querySelectorAll('#dropped, #SecTab [onclick*=dropped]').forEach(
+    i => i.addEventListener('click', () => setPrivacy(true)));
 
-const collectTypeLabels = document.querySelectorAll('.collectType label');
-// 想看 看过 在看 搁置 | 抛弃
-//  0    1    2   3      4
-for (let i = 0; i < 4; i++) {
-    collectTypeLabels[i].addEventListener('click', () => setPrivacy(remain));
-}
+const nonDropSelector = ['wish', 'collect', 'do', 'on_hold'].map(s => `#${s}, #SecTab [onclick*=${s}]`).join(', ');
+document.querySelectorAll(nonDropSelector).forEach(i => i.addEventListener('click', () => setPrivacy(remain)));
 
-if (/collect_type=5\D?/.test(location.search)) {
+if (/collect_type=5/.test(location.search)) {
     setPrivacy(true);
 }
