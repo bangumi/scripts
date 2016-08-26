1c60dae397c99bb3f270778fd6ded1e14259f27a
protodonuts <prevails.z@live.com>
2016-08-26 18:32:46 +0800

    fix compability in firefox

--- prevails/set_privacy_on_drop.user.js
+++ prevails/set_privacy_on_drop.user.js
@@ -3,7 +3,7 @@
 // @namespace    bangumi.scripts.prevails.setprivacyondrop
 // @author       "Donuts."
 // @description  在所有你主动点"抛弃"的时刻, 自动勾选"仅自己可见".
-// @version      1.0.1
+// @version      1.0.2
 // @include      /^https?:\/\/(bgm\.tv|bangumi\.tv|chii\.in)\/(update|subject)\/\d+(\?.*)?$/
 // @grant        none
 // @encoding     utf-8
@@ -18,11 +18,14 @@ function setPrivacy(val) {
     prv.checked = val;
 }
 
-document.querySelectorAll('#dropped, #SecTab [onclick*=dropped]').forEach(
-    i => i.addEventListener('click', () => setPrivacy(true)));
+for (let i of document.querySelectorAll('#dropped, #SecTab [onclick*=dropped]')) {
+	i.addEventListener('click', () => setPrivacy(true));
+}
 
 const nonDropSelector = ['wish', 'collect', 'do', 'on_hold'].map(s => `#${s}, #SecTab [onclick*=${s}]`).join(', ');
-document.querySelectorAll(nonDropSelector).forEach(i => i.addEventListener('click', () => setPrivacy(remain)));
+for (let i of document.querySelectorAll(nonDropSelector)) {
+	i.addEventListener('click', () => setPrivacy(remain));
+}
 
 if (/collect_type=5/.test(location.search)) {
     setPrivacy(true);
