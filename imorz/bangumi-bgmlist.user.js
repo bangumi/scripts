c874dc90df9b4ea9dacc5d7ef5a7f19ce72cc074
sorz <orz@sorz.org>
2017-03-05 22:39:39 +1100

    [Bgmlist] Caching bgmlist data

--- imorz/bangumi-bgmlist.user.js
+++ imorz/bangumi-bgmlist.user.js
@@ -2,17 +2,19 @@
 // @name        Bangumi 国内放送站点链接
 // @description 为 Bangumi 动画条目页左侧添加来自 bgmlist.tv 的国内放送站点及下载搜索链接
 // @namespace   org.sorz.bangumi
-// @grant       none
+// @grant       GM_setValue
+// @grant       GM_getValue
 // @include     /^https?:\/\/((bangumi|bgm)\.tv|chii.in)\/subject\/\d+$/
-// @version     0.1.1
+// @require     http://code.jquery.com/jquery-3.1.1.min.js
+// @version     0.2.0
 // ==/UserScript==
 
-var OLDEST_YEAR = 2013;
-var BGMLIST_URL = 'https://bgmlist.com/tempapi/bangumi/$Y/$M/json';
-var DOWNLOAD_DMHY_URL = "http://share.dmhy.org/topics/list?keyword=";
-var DOWNLOAD_POPGO_URL = "http://share.popgo.org/search.php?title=";
-var DOWNLOAD_NYAA_URL = "http://www.nyaa.se/?page=search&term=";
-var SITES = {
+const OLDEST_YEAR = 2013;
+const CACHE_EXPIRE_SECS = 24 * 3600;
+const BGMLIST_URL = 'https://bgmlist.com/tempapi/bangumi/$Y/$M/json';
+const DOWNLOAD_DMHY_URL = "http://share.dmhy.org/topics/list?keyword=";
+const DOWNLOAD_NYAA_URL = "http://www.nyaa.se/?page=search&term=";
+const SITES = {
   'acfun'   : { name: 'A站' },
   'bilibili': { name: 'B站' },
   'tucao'   : { name: 'C站' },
@@ -27,9 +29,9 @@ var SITES = {
 }
 
 // Change to false to disable download search links:
-var DOWNLOAD_SEARCH_ENABLED = true;
+const DOWNLOAD_SEARCH_ENABLED = true;
 
-var $infobox = $('#infobox');
+let $infobox = $('#infobox');
 
 function $a(href, text) {
   return $('<a>').addClass('l').attr('href', href).text(text);
@@ -51,8 +53,28 @@ function findOnAirYearMonth(callback) {
   });
 }
 
+function getCachedValue(key, maxAge=CACHE_EXPIRE_SECS) {
+  const item = JSON.parse(GM_getValue(key, "{}"));
+  if (!item.cache_store_at ||
+      Date.now() - item.cache_store_at > maxAge * 1000)
+    return;
+  return item.value;
+}
+
+function setCachedValue(key, value) {
+  const item = {
+    'cache_store_at': Date.now(),
+    'value': value
+  };
+  GM_setValue(key, JSON.stringify(item));
+}
+
 function getBgmList(year, month, callback) {
-  let url = BGMLIST_URL.replace('$Y', year).replace('$M', month);
+  const cacheKey = `bgms-${year}-${month}`;
+  const bgms = getCachedValue(cacheKey);
+  if (bgms)
+    return callback(bgms);
+  const url = BGMLIST_URL.replace('$Y', year).replace('$M', month);
   $.getJSON(url, function(list) {
     var bgms = {};
     for (var key in list) {
@@ -62,6 +84,7 @@ function getBgmList(year, month, callback) {
         continue;
       bgms[id] = bgm;
     }
+    setCachedValue(cacheKey, bgms);
     callback(bgms);
   });
 }
