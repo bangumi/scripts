f0ae477dacde31938f8315d2da723cf5cd4dd80e
protodonuts <prevails.z@live.com>
2017-06-22 22:28:39 +0800

    improvement and bug fix

--- prevails/jp_name_kanji_pronunciation.user.js
+++ prevails/jp_name_kanji_pronunciation.user.js
@@ -2,12 +2,12 @@
 // @name        Bangumi 人物名日文汉字标音
 // @include     /^https?:\/\/(bgm\.tv|chii\.in|bangumi\.tv)\/(character|person)\/\d+(\/(collections|works(\/voice)?))?$/
 // @namespace   bangumi.scripts.prevails.mononamerubytag
-// @version     1.0.4
+// @version     1.0.5
 // @author      "Donuts."
 // @grant       none
 // ==/UserScript==
 
-function getRuby(kanji, kana) {
+function ruby(kanji, kana) {
     return `<ruby><rb>${kanji}</rb><rp>（</rp><rt>${kana}</rt><rp>）</rp></ruby>`;
 }
 
@@ -18,10 +18,10 @@ for (let i of document.querySelectorAll('#infobox > li')) {
     const desc = i.getElementsByClassName('tip')[0].innerText;
     if (RegExp('^(别名: )?$').test(desc)) {
         const otherName = i.childNodes[1].wholeText.trim();
-        if (/^[\u3040-\u30ff]+[ 　]+[\u3040-\u30ff]+$/.test(otherName)) {
+        if (/^[\u3040-\u30ff]+\s+[\u3040-\u30ff]+$/.test(otherName)) {
             // 假名 中间空格 假名, 高度符合
             found_h.push(otherName);
-        } else if (/^[\u3040-\u309f]+[ 　]*[\u3040-\u30ff]+$/.test(otherName)) {
+        } else if (/^[\u3040-\u309f]+\s*[\u3040-\u30ff]+$/.test(otherName)) {
             // 平假名 [中间空格] 平假名或片假名, 可能符合(也可能是其它别名字段...)
             found_l.push(otherName);
         }
@@ -38,49 +38,45 @@ if (found_h.length) {
     kana = found_l.pop(); // 规则维持
 } else return;
 
-const [myoujiKana, namaeKana] = kana.split(/[ 　]+/); // namaeKana may be undefined
-
 const nameAnchor = document.querySelector('.nameSingle > a');
 const name = nameAnchor.innerText.trim();
 
 const KANJI_MYOUJI = '[\u4e00-\u9fa5ヶノ々\ufa0e\ufa0f\ufa11\ufa13\ufa14\ufa1f\ufa21\ufa23\ufa24\ufa27\ufa28\ufa29]+';
 const KANJI_NAMAE = '[\u4e00-\u9fa5々\ufa0e\ufa0f\ufa11\ufa13\ufa14\ufa1f\ufa21\ufa23\ufa24\ufa27\ufa28\ufa29]+';
 
-if (RegExp(`^${KANJI_MYOUJI}$`).test(name)) { // 全汉字 无空格, 无法分词
-    nameAnchor.innerHTML = getRuby(name, kana);
-} else if (RegExp(`^${KANJI_MYOUJI}[ 　]+${KANJI_NAMAE}$`).test(name)) { // 全汉字 中间空格
-    if (namaeKana) {
-        const [myouji, namae] = name.split(/[ 　]+/);
-        nameAnchor.innerHTML = getRuby(myouji, myoujiKana) + ' ' + getRuby(namae, namaeKana);
-    } else {
-        nameAnchor.innerHTML = getRuby(name, kana);
-    }
-} else if (RegExp(`^${KANJI_MYOUJI}[ 　]*[\u3040-\u309f][\u3040-\u309f子乃]*$`).test(name)) { // 汉字[空格]平假名
-    const myouji = name.match(RegExp(KANJI_MYOUJI))[0];
-    const namae = name.match(/[\u3040-\u309f][\u3040-\u309f子乃]*/)[0];
-    if (namaeKana) {
-        nameAnchor.innerHTML = getRuby(myouji, myoujiKana) + ' ' + namae;
-    } else {
-        const namaeIndex = kana.lastIndexOf(namae);
-        if (namaeIndex !== -1) {
-            const myoujiKana = kana.slice(0, namaeIndex);
-            nameAnchor.innerHTML = getRuby(myouji, myoujiKana) + ' ' + namae;
+function gen_ruby(name, kana) {
+    const [myoujiKana, namaeKana] = kana.split(/\s+/); // namaeKana may be undefined
+
+    let re;
+    if (RegExp(`^${KANJI_MYOUJI}$`).test(name)) { // 全汉字 无空格, 无法分词
+        return ruby(name, kana);
+    } else if (!!(re = RegExp(`^(${KANJI_MYOUJI})\\s+(${KANJI_NAMAE})$`).exec(name))) { // 全汉字 中间空格, beloved
+        if (namaeKana) {
+            const myouji = re[1];
+            const namae = re[2];
+            return ruby(myouji, myoujiKana) + ' ' + ruby(namae, namaeKana);
         } else {
-            nameAnchor.innerHTML = getRuby(name, kana);
+            return ruby(name, kana);
         }
-    }
-} else if (RegExp(`^${KANJI_MYOUJI}[ 　]*[\u30a0-\u30ff][\u30a0-\u30ff子乃]*$`).test(name)) { // 汉字[空格]片假名
-    const myouji = name.match(RegExp(KANJI_MYOUJI))[0];
-    const namae = name.match(/[\u30a0-\u30ff][\u30a0-\u30ff子乃]*/g).pop(); // "ヶノ" match both KANJI_MYOUJI and \u30a0-\u30ff
-    if (namaeKana) {
-        nameAnchor.innerHTML = getRuby(myouji, myoujiKana) + ' ' + namae;
-    } else {
-        const namaeIndex = kana.lastIndexOf(namae || '蛤');
-        if (namaeIndex !== -1) {
-            const myoujiKana = kana.slice(0, namaeIndex);
-            nameAnchor.innerHTML = getRuby(myouji, myoujiKana) + ' ' + namae;
+    } else if ((re = RegExp(`^(${KANJI_MYOUJI})\\s*([\u3040-\u309f]+[子乃]?)$`).exec(name)) ||  // 汉字[空格]平假名
+               (re = RegExp(`^(${KANJI_MYOUJI})\\s*([\u30a0-\u30ff]+[子乃]?)$`).exec(name))) {  // 汉字[空格]片假名
+        const myouji = re[1];
+        const namae = re[2];
+        if (namaeKana) {
+            return ruby(myouji, myoujiKana) + ' ' + namae;
         } else {
-            nameAnchor.innerHTML = getRuby(name, kana);
+            const namaeIndex = kana.lastIndexOf(namae);
+            if (namaeIndex !== -1) {
+                const myoujiKana = kana.slice(0, namaeIndex);
+                return ruby(myouji, myoujiKana) + ' ' + namae;
+            } else {
+                return ruby(name, kana);
+            }
         }
-    }
+    } else return;
+}
+
+const out = gen_ruby(name, kana);
+if (out) {
+    nameAnchor.innerHTML = out;
 }
