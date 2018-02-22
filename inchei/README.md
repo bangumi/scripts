bfafcd979288a8bc74f365bb94fa900e3d83be5c
inchei <inchei@protonmail.com>
2018-02-22 21:40:48 +0800

    Add bangumi-moveable-thickbox.user.js.

---
+++ inchei/README.md
@@ -0,0 +1,9 @@
+# [inchei](https://bgm.tv/user/inchei)
+
+## [使bangumi的thickbox可移动](https://greasyfork.org/zh-CN/scripts/38584-bangumi-moveable-thickbox)
+
+讨论页：https://bgm.tv/group/topic/345086
+
+使bangumi的thickbox（用于写吐槽的框）可移动，以便在写评分的时候参考他人意见（吐槽箱）
+
+参考：[JavaScript实现最简单的拖拽效果](http://www.zhangxinxu.com/wordpress/2010/03/javascript%E5%AE%9E%E7%8E%B0%E6%9C%80%E7%AE%80%E5%8D%95%E7%9A%84%E6%8B%96%E6%8B%BD%E6%95%88%E6%9E%9C/)（张鑫旭）
---
+++ inchei/bangumi-moveable-thickbox.user.js
@@ -0,0 +1,89 @@
+// ==UserScript==
+// @name         Bangumi Moveable Thickbox
+// @namespace    https://github.com/bangumi/scripts/inchei
+// @version      1.0.0
+// @description  使bangumi的thickbnox可移动
+// @author       inchei
+// @include      /^https?://(bangumi\.tv|bgm\.tv|chii\.in)/.*
+// @grant       GM_addStyle
+// @require https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
+// @require https://cdn.bootcss.com/jqueryui/1.12.1/jquery-ui.min.js
+// ==/UserScript==
+GM_addStyle(`
+    #TB_title {
+        cursor: move;
+        transition: all 0 ease 0;
+        -webkit-transition: all 0 ease 0;
+    }
+`);
+(function() {
+    var TB = document.querySelectorAll(".thickbox");
+    var winHref = window.location.href;
+    var winHrefKey = new RegExp("^https?://((bgm|bangumi).tv|chii.in)/(.*(wish|collect|do|on_hold|dropped))?$");
+    for (var i = 0; i < TB.length; i++) {
+        TB[i].addEventListener("click", function() {
+            if (winHrefKey.test(winHref))
+                $("#TB_window").draggable();
+            else {
+                var params = {
+                    left: 0,
+                    top: 0,
+                    currentX: 0,
+                    currentY: 0,
+                    flag: false
+                };
+                var getCss = function(o, key) {
+                    return o.currentStyle ? o.currentStyle[key] : document.defaultView.getComputedStyle(o, false)[key];
+                };
+                var startDrag = function(bar, target) {
+                    if (getCss(target, "left") !== "auto") {
+                        params.left = getCss(target, "left");
+                    }
+                    if (getCss(target, "top") !== "auto") {
+                        params.top = getCss(target, "top");
+                    }
+                    bar.onmousedown = function(event) {
+                        params.flag = true;
+                        if (!event) {
+                            event = window.event;
+                            bar.onselectstart = function() {
+                                return false;
+                            };
+                        }
+                        var e = event;
+                        params.currentX = e.clientX;
+                        params.currentY = e.clientY;
+                    };
+                    document.onmouseup = function() {
+                        params.flag = false;
+                        if (getCss(target, "left") !== "auto") {
+                            params.left = getCss(target, "left");
+                        }
+                        if (getCss(target, "top") !== "auto") {
+                            params.top = getCss(target, "top");
+                        }
+                    };
+                    document.onmousemove = function(event) {
+                        var e = event ? event : window.event;
+                        if (params.flag) {
+                            var nowX = e.clientX,
+                                nowY = e.clientY;
+                            var disX = nowX - params.currentX,
+                                disY = nowY - params.currentY;
+                            target.style.left = parseInt(params.left) + disX + "px";
+                            target.style.top = parseInt(params.top) + disY + "px";
+                            if (event.preventDefault) {
+                                event.preventDefault();
+                            }
+                            return false;
+                        }
+                    };
+                };
+                var oBox = document.getElementById("TB_window");
+                var oBar = document.getElementById("TB_title");
+                startDrag(oBar, oBox);
+            }
+        });
+
+    }
+})();
