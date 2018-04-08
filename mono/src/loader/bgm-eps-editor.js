69dff2f1c99c44b125fe09a2640815eea2fbdafe
Wang Guan <momocraft@gmail.com>
2018-04-08 09:14:53 +0900

    update SRI in loader

--- mono/src/loader/bgm-eps-editor.js
+++ mono/src/loader/bgm-eps-editor.js
@@ -2,8 +2,8 @@
   var script = document.createElement("script");
   script.crossOrigin = "anonymous";
 
-  script.src = "https://rawgit.com/bangumi/scripts/master/mono/bgm-eps-editor.min.user.js";
-  script.integrity = "sha384-zqTbGecQ6gArPITvoVHEOpjC106CyxOqk63b/Dy79t5Dg5r6kpoNvp+Py+I/IeRU";
+  script.src = "https://cdn.rawgit.com/bangumi/scripts/f425267e44da6c8f6dbfa592a50602984a2e23e9/mono/dist/bgm-eps-editor.min.user.js";
+  script.integrity = "sha384-DtKD6X1yw6ZTTQUTHy2yhkRbzj9548i6CNVKalN/SPTM7EFGal8+K3k6Rjb9MdMi";
 
   document.head.appendChild(script);
 })();
