508740d68f08968ddb637c9a0ac72934ae734860
Wang Guan <momocraft@gmail.com>
2018-10-07 15:52:31 +0900

    create new script

---
+++ mono/dist/bgm-unified-origin.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         bgm-unified-origin
+// @namespace    https://github.com/bangumi/scripts/tree/master/mono
+// @version      1
+// @description  将bangumi各域名链接统一改为当前域名
+// @author       mono <momocraft@gmail.com>
+// @include      /^https?:\/\/(bgm\.tv|chii\.in|bangumi\.tv)\//
+// @grant        none
+// ==/UserScript==
+
+console.log("it works");
--- mono/src/dist.sh
+++ mono/src/dist.sh
@@ -7,7 +7,7 @@ npm run webpack:prod &
 npm run webpack &
 wait
 
-for entry in bgm-eps-editor ; do
+for entry in bgm-eps-editor bgm-unified-origin ; do
   # cat header/$entry.js prod/$entry.min.js > ../dist/$entry.min.user.js
   cat header/$entry.js dev/$entry.js      > ../dist/$entry.user.js
   # cat header/$entry.js loader/$entry.js   > ../dist/$entry.loader.js
