1e367e0dadeff92efd70fdcd9f7fee20e7b47048
Wang Guan <momocraft@gmail.com>
2018-10-07 16:22:59 +0900

    unify origin in links to bgm pages

--- mono/src/dev/bgm-unified-origin.js
+++ mono/src/dev/bgm-unified-origin.js
@@ -1,1 +1,22 @@
-console.log("it works");
+try {
+  if (typeof URL !== 'function') throw "URL api not available";
+  var here = new URL(location.href);
+  document
+    .querySelectorAll('a[href*="bgm.tv" i], a[href*="bangumi.tv" i], a[href*="chii.in" i]')
+    .forEach(function (link) {
+      try {
+        var url = new URL(link.href);
+        if (/^(bangumi\.tv|bgm\.tv|chii\.in)$/i.test(url.host)
+          && (url.host !== here.host || url.protocol !== here.protocol)) {
+          url.host = here.host;
+          url.protocol = here.protocol;
+          link.href = url.toString();
+        }
+      } catch (eSubst) {
+        console.warn("bangumi.tv/dev/app/264 // failed to replace host in ", link);
+      }
+    });
+
+} catch (e) {
+  console.warn("bangumi.tv/dev/app/264 // something went wrong", e);
+}
