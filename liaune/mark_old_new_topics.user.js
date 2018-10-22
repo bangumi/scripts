c4c7a8913cc5d7b696180c623202898628229863
Liaune <1181382547@qq.com>
2018-10-22 14:49:31 +0800

    Update mark_old_new_topics.user.js

--- liaune/mark_old_new_topics.user.js
+++ liaune/mark_old_new_topics.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         坟贴/新贴标志
 // @namespace    https://github.com/bangumi/scripts/liaune
-// @version      1.0
+// @version      1.1
 // @description  在超展开小组页面显示坟贴标志
 // @author       Liaune
 // @include      /^https?://(bangumi\.tv|bgm\.tv|chii\.in)\/.*
@@ -24,11 +24,11 @@
       let id_group = topic.href.split('/group/')[1];
       let id_subject = topic.href.split('/subject/')[1];
       let num = elem.querySelector('.inner small').innerText.match(/\d+/);
-      if(parseInt(id_group)<newest1-1000 || parseInt(id_subject)<newest2-1000) 
+      if(parseInt(id_group)<newest1-3000 || parseInt(id_subject)<newest2-1000) 
         $(`<span style="color: white;background-color:black;border-radius: 4px;">坟</span>`).insertAfter(elem.querySelector('.inner small'));
       else if(parseInt(id_group)>=newest1-5 || parseInt(id_subject)>=newest2-5)
         $(`<span style="color: white;background-color:#ff3ba1;border-radius: 4px;">新</span>`).insertAfter(elem.querySelector('.inner small'));
-      if((parseInt(id_group)>=newest1-600 || parseInt(id_subject)>=newest2-600) && parseInt(num)>100)
+      if((parseInt(id_group)>=newest1-1000 || parseInt(id_subject)>=newest2-600) && parseInt(num)>100)
         $(`<span style="color: white;background-color:red;border-radius: 4px;">火</span>`).insertAfter(elem.querySelector('.inner small'));
     });
   }
