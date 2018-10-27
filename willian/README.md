64500875849e9bc28ca380bec702c55bdeddaf7c
Willian Z <Willian@Willian-Zhang.com>
2018-10-27 16:43:01 -0700

    retina: init

--- willian/README.md
+++ willian/README.md
@@ -3,3 +3,7 @@
 ## [首页条目智能排序](moe.willian.bangumi.sort.user.js?raw=true)
 
 排序首頁的格子, 以特別的順序. 更加便利地知道哪些更新了.
+
+## [首页条目Retina](moe.willian.bangumi.retina.user.js?raw=true)
+
+Retina 化 首頁條目, !對瀏覽器有性能需求.
---
+++ willian/moe.willian.bangumi.retina.user.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         Bangumi Retina Index
+// @namespace    moe.willian.bangumi.retina
+// @version      0.9.8
+// @description  Retinize Index Page for retina
+// @author       Willian
+// @include     http://bangumi.tv/
+// @include     http://bgm.tv/
+// @include     https://bgm.tv/
+// @include     http://chii.in/
+// @run-at       document-end
+// ==/UserScript==
+
+function retinize(){
+    const $ = q => document.querySelector(q);
+    const $All = q => document.querySelectorAll(q);
+
+    // const size = jQuery($('#cloumnSubjectInfo .tinyCover img')).innerHeight();
+    const re = /(\/pic\/cover\/)[gs](\/)/;
+
+    let images = $All('#cloumnSubjectInfo .tinyCover img.grid');
+    for(const img of images){
+        let src = img.getAttribute('src');
+
+        if(re.test(src)){
+            src = src.replace(re, '$1c$2');
+
+            img.classList.add('willian-retina-img-fix', 'willian-size-1');
+            img.removeAttribute('src');
+
+            img.style['background-image'] = `url(${src})`;
+        }
+    }
+
+    images = $All('ul#prgSubjectList li img.grid');
+    for(const img of images){
+        let src = img.getAttribute('src');
+
+        if(re.test(src)){
+            src = src.replace(re, '$1c$2');
+
+            img.classList.add('willian-retina-img-fix', 'willian-size-2');
+            img.removeAttribute('src');
+
+            img.style['background-image'] = `url(${src})`;
+        }
+    }
+
+    images = $All('#cloumnSubjectInfo div.blockMode > .header > a > span > .image > img');
+    for(const img of images){
+        let src = img.getAttribute('src');
+
+        if(re.test(src)){
+            src = src.replace(re, '$1c$2');
+
+            img.classList.add('willian-size-3');
+            img.setAttribute('src', src);
+        }
+    }
+}
+$(document).ready(retinize);
