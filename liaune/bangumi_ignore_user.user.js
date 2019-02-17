79f724e2c401b6c62b9b2f378589ffff656e6d88
Liaune <1181382547@qq.com>
2019-02-17 14:17:06 +0800

    Create bangumi_ignore_user.user.js

---
+++ liaune/bangumi_ignore_user.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         bangumi屏蔽绝交用户的言论
+// @namespace    https://github.com/bangumi/scripts/tree/master/liaune
+// @description   将屏蔽绝交用户的帖子、回复、吐槽等
+// @version      0.1
+// @author       Liaune
+// @include     /^https?://(bgm\.tv|chii\.in|bangumi\.tv)/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    let blacklist;
+    if(localStorage.getItem('bangumi_user_blacklist'))
+        blacklist = JSON.parse(localStorage.getItem('bangumi_user_blacklist'));
+    else
+        blacklist = [];
+    $('.avatarNeue').each(function (){
+        let match = this.style.backgroundImage.match(/(\d+)\.jpg/);
+        let ID = match ? match[1].toString() : null;
+        if(blacklist.includes(ID))
+            $(this.parentNode.parentNode).hide();
+    })
+
+    //设置
+    if(document.location.href.match(/settings\/privacy/)){
+        $('a').each(function (){
+            let match = this.href.match(/(\d+)&gh/);
+            let ID = match ? match[1].toString() : null;
+            if(ID && !blacklist.includes(ID)){
+                blacklist.push(ID);
+                localStorage.setItem('bangumi_user_blacklist',JSON.stringify(blacklist));
+            }
+        })
+    }
+})();
