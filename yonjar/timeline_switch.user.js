da95308bc5625b076b7d09047e41dff0b5776132
yonjar <1332934648@qq.com>
2019-05-13 00:38:36 +0800

    更新为默认开启切换

--- yonjar/timeline_switch.user.js
+++ yonjar/timeline_switch.user.js
@@ -1,25 +1,29 @@
 // ==UserScript==
 // @name         bgm timeline switch
 // @namespace    https://github.com/bangumi/scripts/yonjar
-// @version      0.1.1
+// @version      0.1.2
 // @description  bgm 好友动态&全站动态切换
 // @author       Yonjar
 // @include      /^https?:\/\/(bgm\.tv|chii\.in|bangumi\.tv)\/(timeline)?$/
 // @grant        none
 // ==/UserScript==
 
+let container = document.querySelector("#timelineTabs");
+let btn = document.createElement("li");
+let switchFunc = function() {
+    let timelineContainer = document.querySelector("#timeline ul");
 
-let container = document.querySelector('#timelineTabs');
-let btn = document.createElement('li');
+    fetch(location.origin + "/timeline", { credentials: "omit" })
+        .then(resp => resp.text())
+        .then(html => {
+            let outer = html.match(/<ul>[\s\S]*?<\/ul>/)[0];
+            timelineContainer.outerHTML = outer;
+        });
+
+    document.querySelector("#tmlPager").innerHTML = "";
+};
+
 btn.innerHTML = '<a href="javascript:void(0)">看全站动态</a>';
-btn.addEventListener('click', function () {
-	let timelineContainer = document.querySelector('#timeline ul');
-	fetch(location.origin + '/timeline')
-		.then(resp => resp.text())
-		.then(html => {
-			let outer = html.match(/<ul>[\s\S]*?<\/ul>/)[0];
-			timelineContainer.outerHTML = outer;
-		});
-	document.querySelector('#tmlPager').outerHTML = '';
-}, false);
+btn.addEventListener("click", switchFunc, false);
 container.appendChild(btn);
+switchFunc();
