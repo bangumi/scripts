fe3c4051999ae23a7a1c856b0aa7108ef5d01bcc
yonjar <1332934648@qq.com>
2019-05-26 00:28:20 +0800

    Update timeline_switch.user.js

--- yonjar/timeline_switch.user.js
+++ yonjar/timeline_switch.user.js
@@ -1,29 +1,81 @@
 // ==UserScript==
 // @name         bgm timeline switch
 // @namespace    https://github.com/bangumi/scripts/yonjar
-// @version      0.1.3
+// @version      0.2.1
 // @description  bgm 好友动态&全站动态切换
 // @author       Yonjar
 // @include      /^https?:\/\/(bgm\.tv|chii\.in|bangumi\.tv)\/(timeline)?$/
 // @grant        none
 // ==/UserScript==
 
-let container = document.querySelector("#timelineTabs");
-let btn = document.createElement("li");
-let switchFunc = function() {
-    let timelineContainer = document.querySelector("#timeline ul");
+class timelineSwitch {
+    constructor() {
+        this.tabsContainer = document.querySelector("#timelineTabs");
+        this.flag = localStorage.getItem("timeline_switch_by_yonjar");
+    }
 
-    fetch(location.origin + "/timeline", { credentials: "omit" })
-        .then(resp => resp.text())
-        .then(html => {
-            let outer = html.match(/<ul>[\s\S]*?<\/ul>/)[0];
-            timelineContainer.outerHTML = outer;
+    init() {
+        this.checkLocalStorage();
+        this.createBtn();
+        if (this.flag && this.flag !== "false") {
+            this.switchFunc();
+        }
+    }
+
+    checkLocalStorage() {
+        if (!this.flag) {
+            let f = confirm(
+                "站内动态切换组件: 当前为初次使用, 请设定是否需要默认切换为全站动态?"
+            );
+            localStorage.setItem("timeline_switch_by_yonjar", f);
+        }
+    }
+
+    createBtn() {
+        let liElem = document.createElement("li");
+        liElem.setAttribute("id", "gbtl-btn");
+        liElem.innerHTML = '<a href="javascript:void(0)">看全站动态</a>';
+        liElem.addEventListener("click", this.switchFunc, false);
+        this.tabsContainer.append(liElem);
+    }
+
+    setStyle() {
+        let btn = document.querySelector("#gbtl-btn");
+        this.tabsContainer.querySelectorAll("a.focus").forEach((ele, idx) => {
+            ele.classList.remove("focus");
         });
+        btn.querySelector("a").classList.add("focus");
+    }
 
-    document.querySelector("#tmlPager").innerHTML = "";
-};
+    switchFunc() {
+        //设置按钮样式
+        let timelineContainer = document.querySelector("#timeline");
+        let btn = document.querySelector("#gbtl-btn");
+        let tabsContainer = document.querySelector("#timelineTabs");
 
-btn.innerHTML = '<a href="javascript:void(0)">看全站动态</a>';
-btn.addEventListener("click", switchFunc, false);
-container.appendChild(btn);
-switchFunc();
+        tabsContainer.querySelectorAll("a.focus").forEach((ele, idx) => {
+            ele.classList.remove("focus");
+        });
+        btn.querySelector("a").classList.add("focus");
+
+        fetch(location.origin + "/timeline", { credentials: "omit" })
+            .then(resp => resp.text())
+            .then(html => {
+                let parser = new DOMParser();
+                let doc = parser.parseFromString(html, "text/html");
+
+                let globalTL = doc.querySelector("#timeline");
+                timelineContainer.innerHTML = globalTL.innerHTML;
+            });
+
+        // 移除"下一页"按钮
+        let tmlPager = document.querySelector("#tmlPager");
+        tmlPager.parentNode.removeChild(tmlPager);
+    }
+}
+
+// 判断域名
+if (/^\/(timeline)?$/.test(location.pathname)) {
+    let tl = new timelineSwitch();
+    tl.init();
+}
