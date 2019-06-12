1c2b5108327ea29d136d4b29fdcfc2d33ac031a0
XDcedar <402289412@qq.com>
2019-06-13 01:54:16 +0800

    Update codeblock_prettify.user.js

--- cedar/codeblock_prettify.user.js
+++ cedar/codeblock_prettify.user.js
@@ -4,13 +4,22 @@
 // @version     1.0
 // @description 美化代码块
 // @author      Cedar
-// @include     *
+// @include     /^https?://((bangumi|bgm)\.tv|chii\.in)/.*$/
 // @include     /^https?://((bangumi|bgm)\.tv|chii\.in)/.*#post_\d+$/
 // @include     /^https?://((bangumi|bgm)\.tv|chii\.in)/.*#;$/
 // @include     /^https?://((bangumi|bgm)\.tv|chii\.in)/.*\?.*/
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
+
+  GM_addStyle(`
+div.codeHighlight>pre {
+  font-family: "等距更纱黑体 T SC", consolas, "Lucida Console", Monaco, monospace;
+  font-size: 16px;
+}
+`);
+
   let code = document.getElementsByClassName("codeHighlight");
   for (let i = 0; i < code.length; i++)
     code[i].innerHTML = code[i].innerHTML.replace(/<br>\n/g, "<br>");
