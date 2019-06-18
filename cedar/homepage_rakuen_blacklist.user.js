c9e9f9b82dccb92acc242c5d43ae6e1b41166506
XDcedar <402289412@qq.com>
2019-06-18 20:35:22 +0800

    simplify code. slightly speed up.

--- cedar/homepage_rakuen_blacklist.user.js
+++ cedar/homepage_rakuen_blacklist.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        首页热门条目与小组讨论屏蔽
 // @namespace   tv.bgm.cedar.homepagerakuenblacklist
-// @version     1.0
+// @version     1.0.1
 // @description 根据指定关键词或ID屏蔽首页热门条目与小组讨论
 // @author      Cedar
 // @include     /^https?://((bangumi|bgm)\.tv|chii\.in)$/
@@ -29,6 +29,9 @@
   let blacklist = JSON.parse(localStorage[storageKey] || "{}");
 
   if (location.pathname == "/settings/privacy") {
+    const generateTableRow = (rowname, inputname, inputvalue) => 
+      `<tr><td>${rowname}</td><td><input name="${inputname}" ${inputvalue && inputvalue.length? 'value="'+inputvalue+'" ': ""}class="inputtext" type="text"></td></tr>`;
+
     let formtable =
 `<form id="homepage-rakuen-blacklist">
 <table style="margin:auto; width:98%" class="settings">
@@ -37,35 +40,23 @@
 <tr><td style="width:10%">屏蔽内容</td><td style="width:90%">ID或关键词</td></tr>
 </thead>
 <tbody>
-<tr>
-<td>小组ID</td>
-<td><input name="groupIDs" class="inputtext" type="text" value=${blacklist.groupIDs || ""}></td>
-</tr>
-<tr>
-<td>小组标题</td>
-<td><input name="groupTitleKeywords" class="inputtext" type="text" value=${blacklist.groupTitleKeywords || ""}></td>
-</tr>
-<tr>
-<td>条目ID</td>
-<td><input name="subjectIDs" class="inputtext" type="text" value=${blacklist.subjectIDs || ""}></td>
-</tr>
-<tr>
-<td>条目标题</td>
-<td><input name="subjectTitleKeywords" class="inputtext" type="text" value=${blacklist.subjectTitleKeywords || ""}></td>
-</tr>
+${generateTableRow('小组ID', 'groupIDs', blacklist.groupIDs)
++ generateTableRow('小组标题', 'groupTitleKeywords', blacklist.groupTitleKeywords)
++ generateTableRow('条目ID', 'subjectIDs', blacklist.subjectIDs)
++ generateTableRow('条目标题', 'subjectTitleKeywords', blacklist.subjectTitleKeywords)}
 <tr>
 <td colspan="2"><input class="inputBtn" type="button" style="margin-right: 5px" value="保存"><span style="display:none"></span></td>
 <td></td>
 </tr>
 </tbody>
 </table>
-</form>`
+</form>`;
 
     $(formtable).appendTo(document.getElementById("columnA"));
 
     let form = document.getElementById('homepage-rakuen-blacklist');
     form.getElementsByClassName("inputBtn")[0].addEventListener("click", function() {
-      // 下方代码含义: 把 [["key1", "value1 value2"], ["key2", "value3 value4"]] 变为 {"input1": ["value1", "value2"], "input2": ["value3", "value4"]}
+      // 下方代码含义: 把 [["key1", "value1 value2,"], ["key2", "value3, value4"]] 变为 {"key1": ["value1", "value2"], "key2": ["value3", "value4"]}
       blacklist = Array.from(new FormData(form)).reduce((m, x) => Object.assign( m, {[x[0]]: x[1].split(/[, ]+/).filter(x => x.length)} ), {});
       localStorage[storageKey] = JSON.stringify(blacklist);
       $(this.nextElementSibling).fadeIn("fast").fadeTo(300, 1).fadeOut("slow");
@@ -74,7 +65,8 @@
   }
 
   function filterItems(elemList, elemParser, keywordList, strictMode=false) {
-    // elemParser should return a string.
+    // Note: elemParser should return a string.
+    if (!keywordList.length) return;
     for (let e of elemList) {
       const s = elemParser(e);
       if (keywordList.some(strictMode? (k => s === k): (k => s.includes(k))))
