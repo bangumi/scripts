eca55180aa9c7786cfeb6bff148adbb7c69dde76
XDcedar <402289412@qq.com>
2019-07-03 00:23:15 +0800

    support more pages
    新增支持个人收藏, 目录, Tag, 搜索和排行榜页面

--- cedar/comment_word_counter.user.js
+++ cedar/comment_word_counter.user.js
@@ -1,33 +1,49 @@
 // ==UserScript==
 // @name        简评字数统计
 // @namespace   tv.bgm.cedar.wordcounter
-// @version     1.1
+// @version     1.2
 // @description 统计简评字数
 // @author      Cedar
 // @include     /^https?://((bgm|bangumi)\.tv|chii\.in)/subject/\d+(#;)?$/
+// @include     /^https?://((bgm|bangumi)\.tv|chii\.in)/index/\d+$/
+// @include     /^https?://((bgm|bangumi)\.tv|chii\.in)/.*(browser|tag|subject_search|list)[?/].+$/
 // ==/UserScript==
 
-const total = 200;
-let $comment = $('#comment');
-const getCount = () => $comment.val().length;
+(function() {
+	'use strict';
 
-let $total = $(document.createElement('span')).css('padding', '0 5px').text(total);
-let $wordcounter = $total.clone().text(getCount());
-let $wordcounterWrapper = $(document.createElement('div'))
-	.css('margin-bottom', '8px').append($wordcounter, '/', $total);
-$("#collectBoxForm").children('.clearit').last().before($wordcounterWrapper);
+	function createWordCounter(dom=document) {
+		const total = 200;
+		let $comment = $('#comment', dom);
+		const getCount = () => $comment.val().length;
 
-// "input" event for paste action on mobile.
-$comment.on('blur keyup input', function() {
-	let count = getCount();
-	$wordcounter.text(count);
-	if(count > total)
-		$wordcounter.css("color","red");
-	else
-		$wordcounter.css("color","");
-});
+		let $total = $(document.createElement('span')).css('padding', '0 5px').text(total);
+		let $wordcounter = $total.clone().text(getCount());
+		let $wordcounterWrapper = $(document.createElement('div'))
+			.css('margin-bottom', '8px').append($wordcounter, '/', $total);
+		$("#collectBoxForm", dom).children('.clearit').last().before($wordcounterWrapper);
+
+		// "input" event for paste action on mobile.
+		$comment.on('blur keyup input', function() {
+			let count = getCount();
+			$wordcounter.text(count);
+			if(count > total) $wordcounter.css("color","red");
+			else $wordcounter.css("color","");
+		});
+	}
+
+	function loop() {
+		let ready = $('#TB_iframeContent').length && $('#comment', $('#TB_iframeContent').contents()).length;
+		if(ready) createWordCounter($('#TB_iframeContent').contents());
+		else window.requestAnimationFrame(loop);
+	}
+
+	if(location.pathname.startsWith("/subject/")) createWordCounter()
+	else $('a.thickbox').on('click', function() {window.requestAnimationFrame(loop)})
+}) ();
 
 /** version:
+ *  ver 1.2     支持个人收藏, 目录, Tag, 搜索和排行榜页面
  *  ver 1.1     实现方法优化
  *  ver 1.0.1   修改metadata(@include @namespace)
  *  ver 1.0     初始版本.
