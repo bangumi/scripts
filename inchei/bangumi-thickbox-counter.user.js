e3904c9801d939ee37179f776f05dc08e9a86787
inchei <inchei@protonmail.com>
2019-07-05 21:33:29 +0800

    Update thickbox counter.

--- inchei/bangumi-thickbox-counter.user.js
+++ inchei/bangumi-thickbox-counter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bangumi Thickbox Counter
 // @namespace    https://github.com/bangumi/scripts/tree/master/inchei
-// @version      0.2
+// @version      0.3
 // @description  Counting the rest number of your words in thickbox
 // @icon         https://bgm.tv/img/favicon.ico
 // @author       inchei
@@ -10,43 +10,43 @@
 // ==/UserScript==
 
 (function() {
-    let winHrefKey = new RegExp("^https?://((bgm|bangumi).tv|chii.in)/subject/*");
-    let TB = document.querySelectorAll(".thickbox");
+    const winHrefKey = new RegExp('^https?://((bgm|bangumi).tv|chii.in)/subject/*');
+    const TB = document.querySelectorAll('.thickbox');
 
     function main(textarea, tip) {
         var restNum = 200 - textarea.value.length;
         if (restNum >= 0) {
-            tip.innerText = "吐槽 (简评，剩余 " + restNum + " 字):";
+            tip.style.color = '#666';
+            tip.innerText = '吐槽 (简评，剩余 ' + restNum + ' 字):';
         }
         else {
-            tip.innerText = "吐槽 (简评，超出 " + -restNum + " 字):";
+            tip.style.color = 'red';
+            tip.innerText = '吐槽 (简评，超出 ' + -restNum + ' 字):';
         }
     }
 
     for (var i=0; i<TB.length; i++) {
-        TB[i].addEventListener("click", function() {
+        TB[i].addEventListener('click', () => {
             var textarea;
             var tip;
             if(!winHrefKey.test(window.location.href)) {
-            for (var i=0; i<1000; i++) {
-                setTimeout(function() {
-                    if (document.querySelector("#TB_iframeContent")) {
-                        textarea = document.querySelector("#TB_iframeContent").contentWindow.document.querySelector("textarea#comment.quick");
-                        tip = document.querySelector("#TB_iframeContent").contentWindow.document.querySelector("[for='comment']");
-                        textarea.onkeyup = function() {
-                            var restNum = 200 - textarea.value.length;
+                setInterval(() => {
+                    if (document.querySelector('#TB_iframeContent')) {
+                        textarea = document.querySelector('#TB_iframeContent').contentWindow.document.querySelector('textarea#comment.quick');
+                        tip = document.querySelector('#TB_iframeContent').contentWindow.document.querySelector('[for="comment"]');
+                        main(textarea, tip);
+                        textarea.onkeyup = () => {
                             main(textarea, tip);
-                        }
+                        };
+                        window.clearInterval();
                     }
                 }, 500);
-            }
-        } else {
-            textarea = document.querySelector("textarea#comment.quick");
-            tip = document.querySelector("[for='comment']");
-            textarea.onkeyup = function() {
+            } else {
+                textarea = document.querySelector('textarea#comment.quick');
+                tip = document.querySelector('[for="comment"]');
                 main(textarea, tip);
+                textarea.onkeyup = main(textarea, tip);
             }
-        }
         });
     }
 })();
