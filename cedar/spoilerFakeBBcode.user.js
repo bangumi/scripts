40f4f88ff863dd7c7bf744f587f121ac6cbb64fc
XDcedar <402289412@qq.com>
2019-08-02 15:21:59 +0800

    better CSS, optimize code.

--- cedar/spoilerFakeBBcode.user.js
+++ cedar/spoilerFakeBBcode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        折叠Bangumi
 // @namespace   tv.bgm.cedar.spoilerFakeBBcode
-// @version     0.9
+// @version     1.0
 // @description 折叠Bangumi
 // @author      Cedar
 // @include     /^https?://((bangumi|bgm)\.tv|chii\.in)/.*(topic|ep|blog|comments|character).*/
@@ -21,15 +21,16 @@
 
   GM_addStyle{`
 .spoiler-content {
-  border-style: inset;
-  padding: 0 10px;
-  border-color: lightgrey;
-  background-color: snow;
+  padding: 10px;
+  border: 2px solid #EEE;
+  background-color: #FEFBFC;
+  border-radius: 5px;
 }
 `};
 
   const SEARCHING_HEAD = 0;
   const COLLECTING_DATA = 1;
+  const ASSEMBLING_SPOILER = 2;
 
   // wrapper and button
   const $collapseWrapper = $(document.createElement("div")).addClass("spoiler-content").hide();
@@ -41,55 +42,52 @@
   //const validHead = /^\s*(!?)\*===+\s*((.*?)\s*===+\*\1\s*)?$/;
   //const validTail = /^\s*(!?)\*===+\*\1\s*$/;
   //const getTitle = s => s.replace(validHead, "$3");
+  const keywords = /spoiler|fold|hide/;
   const validHead = /^\s*\[(spoiler|fold|hide)(=(.*))?\]\s*$/;
   const validTail = /^\s*\[\/(spoiler|fold|hide)\]\s*$/
   const getTitle = s => s.replace(validHead, "$3");
   const isBr = el => el && el.nodeType === Node.ELEMENT_NODE && el.tagName === "BR";
 
   function collapse(content) {
-    let flag, startNode, text, title, collapseNodes = [];
-    const reset = () => {flag = SEARCHING_HEAD, startNode = text = title = null, collapseNodes.length = 0};
+    if (!keywords.test(content.innerHTML)) return;
+    let flag, startNode, endNode, title, collapseNodes = [];
+    const reset = () => {flag = SEARCHING_HEAD, startNode = endNode = title = null, collapseNodes.length = 0};
     reset();
 
     let node = content.firstChild;
     while (node) {
       switch (flag) {
         case SEARCHING_HEAD:
-          if(node.nodeType === Node.TEXT_NODE) {
-            text = node.wholeText;
-            if(validHead.test(text)) {
-              startNode = node;
-              title = getTitle(text) || "展开 / 折叠";
-              flag = COLLECTING_DATA;
-            }
+          if(node.nodeType === Node.TEXT_NODE && validHead.test(node.wholeText)) {
+            startNode = node;
+            title = getTitle(node.wholeText) || "展开 / 折叠";
+            flag = COLLECTING_DATA;
           }
           node = node.nextSibling;
           break;
         case COLLECTING_DATA:
-          if(node.nodeType === Node.TEXT_NODE) {
-            text = node.wholeText;
-            if(validTail.test(text)) {
-              if(isBr(collapseNodes[0]))
-                content.removeChild(collapseNodes.shift());
-              if(isBr(collapseNodes[collapseNodes.length-1]))
-                content.removeChild(collapseNodes.pop());
-              $(node).after(
-                $(document.createElement("div")).append(
-                  $collapseBtn.clone(true).text(title),
-                  $collapseWrapper.clone().append(collapseNodes)
-                )
-              )
-              let endNode = node;
-              node = node.nextSibling;
-
-              content.removeChild(endNode);
-              content.removeChild(startNode);
-              reset();
-              break;
-            }
+          if(node.nodeType === Node.TEXT_NODE && validTail.test(node.wholeText)) {
+            endNode = node;
+            flag = ASSEMBLING_SPOILER;
           }
-          collapseNodes.push(node);
-          node = node.nextSibling;
+          else {
+            collapseNodes.push(node);
+            node = node.nextSibling;
+          }
+          break;
+        case ASSEMBLING_SPOILER:
+          if(isBr(collapseNodes[0])) content.removeChild(collapseNodes.shift());
+          if(isBr(collapseNodes[collapseNodes.length-1])) content.removeChild(collapseNodes.pop());
+          $(endNode).after(
+            $(document.createElement("div")).append(
+              $collapseBtn.clone(true).text(title),
+              $collapseWrapper.clone().append(collapseNodes)
+            )
+          )
+          node = node.nextSibling; //摆前面, 因为removeChild后就没有nextSibling了
+          content.removeChild(startNode);
+          content.removeChild(endNode);
+          reset();
           break;
       }
     }
