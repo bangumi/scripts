b1cf0934fd7d25ab4f58859d8325552ce26fc036
XDcedar <402289412@qq.com>
2019-08-03 03:36:14 +0800

    support rakuen.

--- cedar/make_internal_link.user.js
+++ cedar/make_internal_link.user.js
@@ -1,24 +1,52 @@
 // ==UserScript==
 // @name        本页楼层跳转支持
 // @namespace   tv.bgm.cedar.makeInternalLink
-// @version     1.0
+// @version     1.1
 // @description 支持本页楼层跳转
 // @author      Cedar
-// @include     /^https?://(bangumi\.tv|bgm\.tv|chii\.in)/.*(topic|ep|blog|comments|character).*/
+// @include     /^https?://(bangumi\.tv|bgm\.tv|chii\.in)/.*(ep|blog|group|subject|comments|character).*/
+// @include     /^https?://(bangumi\.tv|bgm\.tv|chii\.in)/rakuen/topic/.*/
 // ==/UserScript==
 
 /** include解释:
  *  ep       : 单集评论
  *  blog     : 日志
- *  topic    : 小组帖子 & 条目讨论
+ *  group    : 小组帖子
+ *  subject  : 条目讨论
  *  comments : 目录评论页
  *  character: 角色页
+ *  rakuen/topic/: 超展开
  */
 
 (function() {
   'use strict';
 
-  const isInternalLink = url => /^bangumi\.tv|bgm\.tv|chii\.in$/.test(url.hostname) && url.pathname == location.pathname && url.hash;
+  function isInternalLink(url) {
+    /** rakuen link <-> normal link
+     *  /rakuen/topic/group/{id}   <-> /group/topic/{id}
+     *  /rakuen/topic/subject/{id} <-> /subject/topic/{id}
+     *  /rakuen/topic/ep/{id}      <-> /ep/{id}
+     *  /rakuen/topic/prsn/{id}    <-> /person/{id}
+     *  /rakuen/topic/crt/{id}     <-> /character/{id}
+     */
+    let valid = url.hash && /^bangumi\.tv|bgm\.tv|chii\.in$/.test(url.hostname);
+    if (!valid) return false;
+    if(location.pathname.startsWith("/rakuen/topic/")) {
+      let [type, id] = location.pathname.split("/").slice(-2);
+      switch(type) {
+        case "group":
+        case "subject":
+          return url.pathname === `/${type}/topic/${id}`;
+        case "ep":
+          return url.pathname === "/ep/"+id;
+        case "prsn":
+          return url.pathname === "/person/"+id;
+        case "crt":
+          return url.pathname === "/character/"+id;
+      }
+    }
+    return url.pathname === location.pathname;
+  }
   function makeInternal(a) {
     let url = new URL(a.href);
     if (isInternalLink(url)) {
