9820f402d30ea6383c7bd0db73e134f08020b435
Liaune <1181382547@qq.com>
2019-09-15 20:23:41 +0800

    Update bangumi_related_subject_enhance.user.js

--- liaune/bangumi_related_subject_enhance.user.js
+++ liaune/bangumi_related_subject_enhance.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         bangumi关联条目显示增强
+// @name         Bangumi Related Subject Enhance
 // @namespace    https://github.com/bangumi/scripts/liaune
-// @version      0.4.3
-// @description  显示条目页面关联条目的完成情况
+// @version      0.5
+// @description  显示条目页面关联条目的收藏情况,显示关联条目的排名，单行本设为全部已读/取消全部已读
 // @author       Liaune
 // @include     /^https?:\/\/((bangumi|bgm)\.tv|chii.in)\/subject\/\d+$/
 // @grant        GM_addStyle
 // ==/UserScript==
 (function() {
-    GM_addStyle(`
+ GM_addStyle(`
 .rank{
 padding: 2px 5px 1px 5px;
 background: #b4b020;
@@ -62,11 +62,34 @@ border-width:2px;
 border-radius: 4px
 }
 
+.subCheckIn{
+display:block;
+top: -20px;
+left: 5px;
+opacity: 0.5;
+position: relative;
+padding: 0 2px;
+width: 16px;
+height: 18px;
+background: no-repeat url(/img/ico/ico_eye.png) 50% top;
+}
+
 `);
-    let update=0,count=0;
-    const itemsList = document.querySelectorAll('#columnSubjectHomeB  ul.browserCoverMedium li');
-    const itemsList2 = document.querySelectorAll('#columnSubjectHomeB  ul.coversSmall li');
-    const TotalItems=itemsList.length + itemsList2.length;
+    let collectStatus;
+    let itemsList = document.querySelectorAll('#columnSubjectHomeB  ul.browserCoverMedium li');
+    let itemsList2 = document.querySelectorAll('#columnSubjectHomeB  ul.coversSmall li');
+    let itemsList3 = document.querySelectorAll('#columnSubjectHomeB  ul.browserCoverSmall li');
+    let TotalItems=itemsList.length + itemsList2.length;
+    if(localStorage.getItem('bangumi_subject_collectStatus'))
+        collectStatus = JSON.parse(localStorage.getItem('bangumi_subject_collectStatus'));
+    else
+        collectStatus = {};
+    let securitycode;
+    let badgeUserPanel=document.querySelectorAll('#badgeUserPanel a');
+    badgeUserPanel.forEach( (elem, index) => {
+        if(elem.href.match(/logout/))
+            securitycode = elem.href.split('/logout/')[1].toString();
+    });
 
     //更新缓存数据
     const showBtn = document.createElement('a');
@@ -74,19 +97,91 @@ border-radius: 4px
     showBtn.className = 'chiiBtn';
     showBtn.href='javascript:;';
     showBtn.textContent = '更新';
+    if(itemsList3.length)
+        document.querySelectorAll('#columnSubjectHomeB .subject_section .clearit')[1].append(showBtn);
+    else
+        document.querySelectorAll('#columnSubjectHomeB .subject_section .clearit')[0].append(showBtn);
 
+    let update=0,count=0;
     GetInfo(update);
-
     function Update(){
         count=0;
         update=1;
         GetInfo(update);
     }
+    let privacy;
+    let privatebox = document.createElement('a');
+    privatebox.textContent = '私密';
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    privatebox.appendChild(checkbox);
+    if(itemsList3.length)
+        $(privatebox).insertAfter(document.querySelectorAll('#columnSubjectHomeB .subject_section .subtitle')[1]);
+    checkbox.onclick = function (){
+     if (checkbox.checked) privacy = 1;
+      else privacy = 0;
+    };
+    const allCollect = document.createElement('a');
+    allCollect.className = 'chiiBtn';
+    allCollect.href='javascript:;';
+    allCollect.textContent = '全部标为已读';
+    let flag = 0;
+    allCollect.onclick = function (){
+        let i = 0;
+        flag = (flag==1)?0:1;
+        allCollect.textContent =(flag==1)? '全部取消已读':'全部标为已读';
+        let getitemsList3= setInterval(function(){
+            let elem = itemsList3[i];
+            let href = elem.querySelector('a.avatar').href;
+            let ID = href.split('/subject/')[1];
+            let avatarNeue = elem.querySelector('span.avatarNeue');
+            if(flag){
+                collectStatus[ID] = 'collect';
+                avatarNeue.classList.add('collect');
+                $.post('/subject/' + ID + '/interest/update?gh=' + securitycode, { status: 'collect',privacy:privacy});
+            }
+            else{
+                delete collectStatus[ID];
+                avatarNeue.classList.remove('collect');
+                $.post('/subject/' + ID + '/remove?gh=' + securitycode);
+            }
+            localStorage.setItem('bangumi_subject_collectStatus',JSON.stringify(collectStatus));
+            i++;
+            if(i >= itemsList3.length){
+                clearInterval(getitemsList3);
+            }
+        },300);
+    };
+    if(itemsList3.length)
+        $(allCollect).insertAfter(document.querySelectorAll('#columnSubjectHomeB .subject_section .subtitle')[1]);
+
+    function ShowCheckIn(elem,ID){
+        let checkIn = document.createElement('a');
+        checkIn.className = 'subCheckIn';
+        checkIn.href='javascript:;';
+        checkIn.addEventListener('click', function(){
+            checkIn.style.backgroundPosition= "bottom left";
+            let avatarNeue = elem.querySelector('span.avatarNeue');
+            avatarNeue.classList.add('collect');
+            collectStatus[ID] = 'collect';
+            localStorage.setItem('bangumi_subject_collectStatus',JSON.stringify(collectStatus));
+            $.post('/subject/' + ID + '/interest/update?gh=' + securitycode, { status: 'collect',privacy:privacy});
+        });
+        elem.querySelector('a.avatar').append(checkIn);
+    }
 
     function GetInfo(update){
-
         itemsList.forEach( (elem, index) => {
             elem.style.height="150px";
+        });
+        itemsList2.forEach( (elem, index) => {
+            elem.style.height="150px";
+            //elem.style.width="82px";
+        });
+        let i = 0;
+        let getitemsList= setInterval(function(){
+            let elem = itemsList[i];
+            let index = i;
             let href = elem.querySelector('a.avatar').href;
             let href1 = href.replace(/subject/,"update");
             let ID = href.split('/subject/')[1];
@@ -95,14 +190,20 @@ border-radius: 4px
                 DisplayRank(info.rankNum,index,1);
             }
             else ShowRank(href,index,1);
-            if(localStorage.getItem(ID+'Interest') && !update)
-                DisplayCollect(localStorage.getItem(ID+'Interest'),index,1);
-            else  ShowCollect(href1,index,1);
-        });
-
-        itemsList2.forEach( (elem, index) => {
-            elem.style.height="150px";
-            elem.style.width="82px";
+            if(collectStatus[ID]!='collect')
+                ShowCheckIn(elem,ID);
+            if(collectStatus[ID] && !update)
+                DisplayCollect(collectStatus[ID],index,1);
+            else ShowCollect(href1,index,1);
+            i++;
+            if(i >= itemsList.length){
+                clearInterval(getitemsList);
+            }
+        },300);
+        let j = 0;
+        let getitemsList2= setInterval(function(){
+            let elem = itemsList2[j];
+            let index = j;
             let href = elem.querySelector('a').href;
             let href1 = href.replace(/subject/,"update");
             let ID = href.split('/subject/')[1];
@@ -111,10 +212,23 @@ border-radius: 4px
                 DisplayRank(info.rankNum,index,0);
             }
             else ShowRank(href,index,0);
-            if(localStorage.getItem(ID+'Interest') && !update)
-                DisplayCollect(localStorage.getItem(ID+'Interest'),index,0);
+            if(collectStatus[ID]!='collect')
+                ShowCheckIn(elem,ID);
+            if(collectStatus[ID] && !update)
+                DisplayCollect(collectStatus[ID],index,0);
             else  ShowCollect(href1,index,0);
-
+            j++;
+            if(j >= itemsList2.length){
+                clearInterval(getitemsList2);
+            }
+        },300);
+        itemsList3.forEach( (elem, index) => {
+            let href = elem.querySelector('a').href;
+            let ID = href.split('/subject/')[1];
+            if(collectStatus[ID])
+                DisplayCollect(collectStatus[ID],index,2);
+            else if(collectStatus[ID]!='collect')
+                ShowCheckIn(elem,ID);
         });
 
         let thisItem = window.location.href.replace(/subject/,"update");
@@ -129,7 +243,9 @@ border-radius: 4px
             let Match = targetStr.match(/"GenInterestBox\('(\S+?)'\)" checked="checked"/);
             let interest = Match ? Match[1] : null;
             let ID = thisItem.split('/update/')[1];
-            if(Match)  localStorage.setItem(ID+'Interest',interest);
+            if(interest) collectStatus[ID] = interest;
+            else if(collectStatus[ID]) delete collectStatus[ID];
+            localStorage.setItem('bangumi_subject_collectStatus',JSON.stringify(collectStatus));
         });
     }
 
@@ -145,39 +261,41 @@ border-radius: 4px
             let Match = targetStr.match(/"GenInterestBox\('(\S+?)'\)" checked="checked"/);
             let interest = Match ? Match[1] : null;
             let ID = href.split('/update/')[1];
-            if(Match)  localStorage.setItem(ID+'Interest',interest);
+            if(Match){
+                collectStatus[ID] = 'collect';
+                localStorage.setItem('bangumi_subject_collectStatus',JSON.stringify(collectStatus));
+            }
             if(!update) DisplayCollect(interest,index,args);
             else{
                 count+=1;
                 showBtn.textContent='更新中... (' + count + '/' + TotalItems +')';
-                if(count==TotalItems){ location.reload(); showBtn4.textContent='更新完毕！';}
+                if(count==TotalItems){ location.reload(); showBtn.textContent='更新完毕！';}
             }
         });
     }
 
     function DisplayCollect(interest,index,args){
-        let avatarNeue,pictureFrameGroup;
-        if(args) avatarNeue = document.querySelectorAll('#columnSubjectHomeB  ul.browserCoverMedium li')[index].querySelector('span.avatarNeue');
-        else pictureFrameGroup = document.querySelectorAll('#columnSubjectHomeB  ul.coversSmall li')[index].querySelector('span.avatarNeue');
+        let avatarNeue;
+        if(args==0)
+            avatarNeue = document.querySelectorAll('#columnSubjectHomeB  ul.coversSmall li')[index].querySelector('span.avatarNeue');
+        else if(args==1)
+            avatarNeue= document.querySelectorAll('#columnSubjectHomeB  ul.browserCoverMedium li')[index].querySelector('span.avatarNeue');
+        else if(args==2)
+            avatarNeue = document.querySelectorAll('#columnSubjectHomeB  ul.browserCoverSmall li')[index].querySelector('span.avatarNeue');
         if(interest=='wish'){
-            if(args) avatarNeue.classList.add('wish');
-            else pictureFrameGroup.classList.add('wish');
+            avatarNeue.classList.add('wish');
         }
         else if(interest=='collect'){
-            if(args) avatarNeue.classList.add('collect');
-            else pictureFrameGroup.classList.add('collect');
+            avatarNeue.classList.add('collect');
         }
         else if(interest=='do'){
-            if(args) avatarNeue.classList.add('do');
-            else pictureFrameGroup.classList.add('do');
+            avatarNeue.classList.add('do');
         }
         else if(interest=='on_hold'){
-            if(args) avatarNeue.classList.add('on_hold');
-            else pictureFrameGroup.classList.add('on_hold');
+            avatarNeue.classList.add('on_hold');
         }
         else if(interest=='dropped'){
-            if(args) avatarNeue.classList.add('dropped');
-            else pictureFrameGroup.classList.add('dropped');
+            avatarNeue.classList.add('dropped');
         }
     }
 
@@ -215,7 +333,7 @@ border-radius: 4px
             if(!update) DisplayRank(rankNum,index,args);
             else{
                 showBtn.textContent='更新中... (' + count + '/' + TotalItems +')';
-                if(count==itemsList.length){ location.reload(); showBtn4.textContent='更新完毕！';}
+                if(count==itemsList.length){ location.reload(); showBtn.textContent='更新完毕！';}
             }
         });
     }
@@ -233,8 +351,6 @@ border-radius: 4px
         if(args)  document.querySelectorAll('#columnSubjectHomeB  ul.browserCoverMedium li')[index].append(rankSp);
         else document.querySelectorAll('#columnSubjectHomeB  ul.coversSmall li')[index].append(rankSp);
         count+=1;
-        if(count==TotalItems && document.querySelector('#columnSubjectHomeB .subject_section .clearit'))
-            document.querySelector('#columnSubjectHomeB .subject_section .clearit').append(showBtn);
     }
 
 })();
