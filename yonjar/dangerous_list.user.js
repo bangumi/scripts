3e6daf440157dfa4156a029c150538fb413c9b6b
yonjar <1332934648@qq.com>
2020-08-08 14:30:41 +0800

    Update dangerous_list.user.js
    添加简易屏蔽功能

--- yonjar/dangerous_list.user.js
+++ yonjar/dangerous_list.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         bangumi危险名单
 // @namespace    https://github.com/bangumi/scripts/yonjar
-// @version      0.1.2
-// @description  bangumi危险名单
+// @version      0.2.1
+// @description  bangumi危险名单&简易屏蔽
 // @author       Yonjar
 // @include      /^https?:\/\/(bgm\.tv|chii\.in|bangumi\.tv)\/(user|blog|ep|character|person|(group|subject)\/topic|rakuen\/topic\/(ep|crt|group|subject|prsn))\/\S+(\?.*)?(#.*)?$/
 // @grant        GM_addStyle
@@ -20,88 +20,124 @@ GM_addStyle(`
     `);
 
 class DangerousList {
-    constructor(){
-        if (!localStorage.getItem('dangerous_list_by_yonjar')) {
-            localStorage.setItem('dangerous_list_by_yonjar', JSON.stringify([]));
-        }
-        this.userList = JSON.parse(localStorage.getItem('dangerous_list_by_yonjar'));
+  constructor() {
+    if (!localStorage.getItem("dangerous_list_by_yonjar")) {
+      localStorage.setItem("dangerous_list_by_yonjar", JSON.stringify([]));
     }
+    this.userList = JSON.parse(
+      localStorage.getItem("dangerous_list_by_yonjar")
+    );
+  }
 
-    get list(){
-        return this.userList;
-    }
+  get list() {
+    return this.userList;
+  }
 
-    update(){
-        localStorage.setItem('dangerous_list_by_yonjar', JSON.stringify(this.userList));
-    }
+  update() {
+    localStorage.setItem(
+      "dangerous_list_by_yonjar",
+      JSON.stringify(this.userList)
+    );
+  }
 
-    add(userId){
-        this.userList.push(userId);
-        this.update();
-    }
+  add(userId) {
+    this.userList.push(userId);
+    this.update();
+  }
 
-    remove(userId){
-        for (let i = 0; i < this.userList.length; i++){
-            if (this.userList[i] === userId) {
-                this.userList.splice(i, 1);
-                break;
-            }
-        }
-        this.update();
+  remove(userId) {
+    for (let i = 0; i < this.userList.length; i++) {
+      if (this.userList[i] === userId) {
+        this.userList.splice(i, 1);
+        break;
+      }
     }
+    this.update();
+  }
 
-    has(userId){
-        return this.userList.includes(userId);
-    }
+  has(userId) {
+    return this.userList.includes(userId);
+  }
 }
 
 class User {
-    constructor(){
-        this.id = location.pathname.split('user/')[1];
-    }
+  constructor() {
+    this.id = location.pathname.split("user/")[1];
+    this.name = document.querySelector(
+      "#headerProfile h1 > div.inner > a"
+    ).textContent;
+  }
 
-    init(){
-        let dl = new DangerousList();
-        let dl_btn = document.createElement('a');
-        dl_btn.classList.add('chiiBtn');
-        dl_btn.classList.add('yonjar_bgm_dangerous_list_btn');
-        dl_btn.innerHTML = dl.has(this.id) ? '<span>移出危险名单</span>' : '<span>加入危险名单</span>';
-        dl_btn.addEventListener('click', (e) => {
-            e.preventDefault();
-            if (dl.has(this.id)) {
-                dl.remove(this.id);
-                dl_btn.innerHTML = '<span>加入危险名单</span>';
-            }
-            else {
-                dl.add(this.id);
-                dl_btn.innerHTML = '<span>移出危险名单</span>';
-            }
-        });
+  init() {
+    let dl = new DangerousList();
 
-        let titleElem = document.querySelector('div.headerContainer > h1 > div.rr');
-        titleElem.appendChild(dl_btn);
-    }
+    // 危险名单btn
+    let dl_btn = document.createElement("a");
+    dl_btn.classList.add("chiiBtn");
+    dl_btn.classList.add("yonjar_bgm_dangerous_list_btn");
+    dl_btn.innerHTML = dl.has(this.id)
+      ? "<span>移出危险名单</span>"
+      : "<span>加入危险名单</span>";
+    dl_btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (dl.has(this.id)) {
+        dl.remove(this.id);
+        dl_btn.innerHTML = "<span>加入危险名单</span>";
+      } else {
+        dl.add(this.id);
+        dl_btn.innerHTML = "<span>移出危险名单</span>";
+      }
+    });
 
+    // 屏蔽btn
+    let block_btn = document.createElement("a");
+    block_btn.classList.add("chiiBtn");
+    block_btn.classList.add("yonjar_bgm_dangerous_list_btn");
+    block_btn.innerHTML = "<span>在下面生成ublock规则</span>";
+    block_btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(
+        ".bio"
+      ).textContent = `! https://bgm.tv 屏蔽首页侧边栏&超展开&小组页面的帖子
+      bgm.tv##li:has(> a[title="${this.name}"])
+      bgm.tv##li:has(img[title="${this.name}"])
+      bgm.tv##tr:has(a[href="/user/${this.id}"])
+      
+      ! https://bgm.tv 屏蔽贴内的回复
+      bgm.tv##.topic_sub_reply > div:has(> a[href="/user/${this.id}"])
+      bgm.tv##.row_reply:has(> a[href="/user/${this.id}"])
+      `;
+      document.querySelector(".bio").style.whiteSpace = "pre-line";
+    });
+
+    let titleElem = document.querySelector("div.headerContainer > h1 > div.rr");
+    titleElem.appendChild(dl_btn);
+    titleElem.appendChild(block_btn);
+  }
 }
 
-(function(){
-    let cur_url = location.href;
-    if (/^https?:\/\/(bgm\.tv|chii\.in|bangumi\.tv)\/user\/[^\/]+$/.test(cur_url)) {
-        let user = new User();
-        user.init();
-        return;
+(function () {
+  let cur_url = location.href;
+  if (
+    /^https?:\/\/(bgm\.tv|chii\.in|bangumi\.tv)\/user\/[^\/]+$/.test(cur_url)
+  ) {
+    let user = new User();
+    user.init();
+    return;
+  }
+
+  let localData = localStorage.getItem("dangerous_list_by_yonjar");
+  let dangerousList = JSON.parse(localData);
+
+  let commentUsers = document.querySelectorAll(
+    "#comment_list div.inner strong a"
+  );
+
+  commentUsers.forEach((e) => {
+    let userId = e.href.split("user/")[1];
+    if (dangerousList.includes(userId)) {
+      e.style.color = "#f1cfcf";
+      e.style.background = "#ff2424";
     }
-
-    let localData = localStorage.getItem('dangerous_list_by_yonjar');
-    let dangerousList = JSON.parse(localData);
-
-    let commentUsers = document.querySelectorAll('#comment_list div.inner strong a');
-
-    commentUsers.forEach( e => {
-        let userId = e.href.split('user/')[1];
-        if(dangerousList.includes(userId)){
-            e.style.color = '#f1cfcf';
-            e.style.background = '#ff2424';
-        }
-    });
+  });
 })();
