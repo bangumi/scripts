671f4dbc382b9f6a375ca6f49434273e87774d3b
XDcedar <402289412@qq.com>
2020-09-05 03:49:32 +0800

    better comments and code

--- cedar/make_internal_link.user.js
+++ cedar/make_internal_link.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        本页楼层跳转支持
 // @namespace   tv.bgm.cedar.makeInternalLink
-// @version     1.1
+// @version     1.1.1
 // @description 支持本页楼层跳转
 // @author      Cedar
-// @include     /^https?://(bangumi\.tv|bgm\.tv|chii\.in)/.*(ep|blog|group|subject|comments|character).*/
+// @include     /^https?://(bangumi\.tv|bgm\.tv|chii\.in)/.*(ep|blog|group|subject|comments|character|person).*/
 // @include     /^https?://(bangumi\.tv|bgm\.tv|chii\.in)/rakuen/topic/.*/
 // ==/UserScript==
 
@@ -14,8 +14,10 @@
  *  group    : 小组帖子
  *  subject  : 条目讨论
  *  comments : 目录评论页
- *  character: 角色页
- *  rakuen/topic/: 超展开
+ *  character: 虚拟角色页
+ *  person   : 现实人物页
+ *  rakuen/topic/(crt|prsn): 超展开的虚拟角色和现实人物页
+ *  rakuen/topic/(ep|group|subject): 超展开的单集评论 小组帖子 条目讨论.
  */
 
 (function() {
@@ -29,8 +31,7 @@
      *  /rakuen/topic/prsn/{id}    <-> /person/{id}
      *  /rakuen/topic/crt/{id}     <-> /character/{id}
      */
-    let valid = url.hash && /^bangumi\.tv|bgm\.tv|chii\.in$/.test(url.hostname);
-    if (!valid) return false;
+    if (!url.hash || !/^bangumi\.tv|bgm\.tv|chii\.in$/.test(url.hostname)) return false;
     if(location.pathname.startsWith("/rakuen/topic/")) {
       let [type, id] = location.pathname.split("/").slice(-2);
       switch(type) {
@@ -48,16 +49,15 @@
     return url.pathname === location.pathname;
   }
   function makeInternal(a) {
-    let url = new URL(a.href);
-    if (isInternalLink(url)) {
-      a.href = url.hash;
+    if (isInternalLink(a)) {
+      a.href = a.hash;
       a.target = "_self";
     }
   }
 
-  document.querySelectorAll(".message a, .cmt_sub_content a").forEach(makeInternal); //所有回复
+  document.querySelectorAll(".message a, .cmt_sub_content a").forEach(makeInternal); //所有回复 (ep和comments页面只需适配所有回复)
   if (window.location.pathname.includes("blog"))  //日志
     document.querySelectorAll(".blog_entry a").forEach(makeInternal);
-  else if (window.location.pathname.includes("topic")) //小组
+  else if (/group|subject/.test(window.location.pathname)) //小组帖子 & 条目讨论 (已包括超展开)
     document.querySelectorAll(".topic_content a").forEach(makeInternal);
 }) ();
