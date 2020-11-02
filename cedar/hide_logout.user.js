425d06ffac660c62ec2fe5675362a458a176a130
XDcedar <402289412@qq.com>
2020-11-02 17:40:18 +0800

    simplify code to be compatible with other gadgets

--- cedar/hide_logout.user.js
+++ cedar/hide_logout.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        登出按钮隐藏
 // @namespace   tv.bgm.cedar.hidelogout
-// @version     1.2
+// @version     1.2.2
 // @description 隐藏登出按钮. 可从设置页登出.
 // @author      Cedar
 // @include     *
@@ -11,30 +11,17 @@
 // @exclude     /^https?://((bangumi|bgm)\.tv|chii\.in)/settings.*$/
 // ==/UserScript==
 
-(function () {
-  'use strict';
+'use strict';
 
-	function findLogoutButton(parentNode) {
-		for (let i = parentNode.lastChild; i; i = i.previousSibling)
-			if (i.nodeType === 1 && i.href.includes("logout"))
-				return i;
-	}
+let dock = (location.pathname.startsWith('/rakuen')? parent.document: document) // 适配超展开
+  .getElementById("dock");
+let logout = dock.querySelector('a[href*="/logout"]');
+logout.style.display = "none";
+logout.previousSibling.replaceWith('\n'); //保持元素个数一致
 
-	let dockNode = (location.pathname.startsWith('/rakuen')? parent.document: document)
-		.querySelector("#dock li.last").previousElementSibling;
-	let t = findLogoutButton(dockNode);
-	t.previousSibling.replaceWith('\n'); //保持元素个数一致
-	t.style.display = "none";
-
-	let badgeUserPanel = document.getElementById("badgeUserPanel").lastElementChild;
-	t = findLogoutButton(badgeUserPanel);
-	t.href = "https://bgm.tv/notify/all";
-	t.innerHTML = "提醒";
-}) ();
-
-/** version:
- *  ver 1.2     支持超展开
- *  ver 1.1     实现方法优化
- *  ver 1.0.1   修改@include
- *  ver 1.0     初始版本
- */
+if(!location.pathname.startsWith('/rakuen')) {
+  let badgeUserPanel = document.getElementById("badgeUserPanel");
+  logout = badgeUserPanel.querySelector('a[href*="/logout"]');
+  logout.href = "https://bgm.tv/notify/all";
+  logout.innerHTML = "提醒";
+}
