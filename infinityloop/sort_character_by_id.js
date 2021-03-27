b9ee1ab6960518ee6224e3a415843c04fe02d0e9
InfinityLoop <swsoyee@gmail.com>
2021-03-27 15:37:52 +0900

    feat: support sort by character id in works of caster page

---
+++ infinityloop/sort_character_by_id.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Bangumi 声优角色按 ID 降序显示
+// @namespace    https://github.com/bangumi/scripts/tree/master/infinityloop
+// @version      0.1.0
+// @icon         http://bangumi.tv/img/favicon.ico
+// @author       InfinityLoop
+// @description  对 Bangumi 中声优页面下的所演出角色按 ID 降序显示。
+// @include      /^https?://((bangumi|bgm)|chii\.in)\.tv/person/\d+/works/voice/
+// @supportURL   https://github.com/bangumi/scripts/issues/new?title=[%E5%A3%B0%E4%BC%98%E4%BD%9C%E5%93%81ID%E9%99%8D%E5%BA%8F]&body=@swsoyee%3Ch2%3E%E9%97%AE%E9%A2%98%E6%8A%A5%E5%91%8A%3C/h2%3E
+// @compatible   chrome
+// @compatible   firefox
+// @compatible   edge
+// @license      MIT
+// ==/UserScript==
+/* globals $ */
+
+(function() {
+    'use strict';
+    const getId = (node) => {
+        return (Number(node.querySelector("div>a").href.split("/").pop()));
+    }
+
+    const sort_li = (prev, cur) => {
+        return (getId(cur) > getId(prev) ? 1 : -1);
+      }
+    $("li.item.clearit").sort(sort_li).appendTo("ul.browserList");
+    $("li.item.clearit").each((index, element) => {
+        $(element).attr("class", index % 2 == 0 ? 'item even clearit' : 'item odd clearit');
+    })
+})();
