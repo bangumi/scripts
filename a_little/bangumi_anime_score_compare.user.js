b22a9a4fa6bce93a3758c257bbf8a96bbacac251
22earth <zhifengle@gmail.com>
2022-06-25 17:59:05 +0800

    [BASC] fixed: 豆瓣没有显示其它网站的评分

--- a_little/bangumi_anime_score_compare.user.js
+++ a_little/bangumi_anime_score_compare.user.js
@@ -2,13 +2,13 @@
 // @name        bangumi anime score compare
 // @name:zh-CN  bangumi动画和豆瓣及MAL评分对比
 // @namespace   https://github.com/22earth
-// @description show subject score information of douban and MAL in bangumi.tv
-// @description:zh-cn bangumi动画页面显示豆瓣和MAL的评分
+// @description show subject score information of douban and MAL in Bangumi or Douban
+// @description:zh-cn 在Bangumi或者豆瓣上面显示其它动画评分网站的评分
 // @author      22earth
 // @homepage    https://github.com/22earth/gm_scripts
 // @include     /^https?:\/\/(bangumi|bgm|chii)\.(tv|in)\/subject\/.*$/
 // @include     https://movie.douban.com/subject/*
-// @version     0.3.3
+// @version     0.3.4
 // @note        0.3.3 支持 Anidb
 // @note        0.2.0 支持豆瓣上显示Bangumi评分,暂时禁用豆瓣上显示MAL的评分功能以及修改过滤方式
 // @note        0.2.4 豆瓣 api 失效，使用搜索页面查询结果
@@ -926,6 +926,11 @@ const DoubanScorePage = {
         {
             selector: 'body',
             subSelector: '.tags-body',
+            keyWord: ['动画', '动漫'],
+        },
+        {
+            selector: '#info',
+            subSelector: 'span[property="v:genre"]',
             keyWord: ['动画', '动漫'],
         },
     ],
