6c7726b399762375698c61a888ca1e0494a77308
XDcedar <402289412@qq.com>
2024-04-27 00:17:16 +0800

    spoilerFakeBBcode: refactor，移除最外层的匿名函数

--- cedar/spoilerFakeBBcode.user.js
+++ cedar/spoilerFakeBBcode.user.js
@@ -21,12 +21,11 @@
  *  rakuen/topic/: 超展开
  */
 
-(function() {
-  'use strict';
+'use strict';
 
-  if (!/^\/.*(ep|blog|group|subject|index|index\/\d+\/comments|character|rakuen\/topic\/).*/.test(location.pathname)) return;
+if (!/^\/.*(ep|blog|group|subject|index|index\/\d+\/comments|character|rakuen\/topic\/).*/.test(location.pathname)) return;
 
-  GM_addStyle(`
+GM_addStyle(`
 .spoiler-wrapper {
   padding: 10px;
   border: 2px solid #EEE;
@@ -51,94 +50,93 @@ html[data-theme='dark'] .spoiler-content {
 }
 `);
 
-  const SEARCHING_HEAD = 0;
-  const COLLECTING_DATA = 1;
-  const ASSEMBLING_SPOILER = 2;
+const SEARCHING_HEAD = 0;
+const COLLECTING_DATA = 1;
+const ASSEMBLING_SPOILER = 2;
 
-  const keywords = /spoiler|fold|hide/i;
-  const validHead = /^\s*\[(spoiler|fold|hide)(=(.*))?\]\s*$/i;
-  const validTail = /^\s*\[\/(spoiler|fold|hide)\]\s*$/i;
-  const getTitle = s => s.match(validHead)[3];
+const keywords = /spoiler|fold|hide/i;
+const validHead = /^\s*\[(spoiler|fold|hide)(=(.*))?\]\s*$/i;
+const validTail = /^\s*\[\/(spoiler|fold|hide)\]\s*$/i;
+const getTitle = s => s.match(validHead)[3];
 
-  function isBr(el) {
-    return el && el.nodeType === Node.ELEMENT_NODE && el.tagName === "BR";
-  }
+function isBr(el) {
+  return el && el.nodeType === Node.ELEMENT_NODE && el.tagName === "BR";
+}
 
-  // 用于组装折叠元素
-  function getCollapseEl(title, collapseNodes) {
-    const summaryEl = document.createElement('summary');
-    summaryEl.style.fontWeight = "bold";
-    summaryEl.style.cursor = "pointer";
-    summaryEl.innerText = title;
-    const contentEl = document.createElement("div");
-    contentEl.classList.add("spoiler-content");
-    contentEl.append(...collapseNodes);
-    const collapseWrapper = document.createElement("details");
-    collapseWrapper.classList.add("spoiler-wrapper");
-    collapseWrapper.append(summaryEl, contentEl);
+// 用于组装折叠元素
+function getCollapseEl(title, collapseNodes) {
+  const summaryEl = document.createElement('summary');
+  summaryEl.style.fontWeight = "bold";
+  summaryEl.style.cursor = "pointer";
+  summaryEl.innerText = title;
+  const contentEl = document.createElement("div");
+  contentEl.classList.add("spoiler-content");
+  contentEl.append(...collapseNodes);
+  const collapseWrapper = document.createElement("details");
+  collapseWrapper.classList.add("spoiler-wrapper");
+  collapseWrapper.append(summaryEl, contentEl);
 
-    return collapseWrapper;
-  }
+  return collapseWrapper;
+}
 
-  function collapse(parentEl) {
-    if (!keywords.test(parentEl.innerHTML)) return;
+function collapse(parentEl) {
+  if (!keywords.test(parentEl.innerHTML)) return;
 
-    let flag, startNode, endNode, title, collapseNodes = [];
+  let flag, startNode, endNode, title, collapseNodes = [];
 
-    const reset = () => {
-      flag = SEARCHING_HEAD;
-      startNode = endNode = title = null;
-      collapseNodes.length = 0
-    };
-    reset();
+  const reset = () => {
+    flag = SEARCHING_HEAD;
+    startNode = endNode = title = null;
+    collapseNodes.length = 0
+  };
+  reset();
 
-    let node = parentEl.firstChild;
-    while (node) {
-      switch (flag) {
-        case SEARCHING_HEAD: {
-          if(node.nodeType === Node.TEXT_NODE && validHead.test(node.wholeText)) {
-            startNode = node;
-            title = getTitle(node.wholeText) || "展开 / 折叠";
-            flag = COLLECTING_DATA;
-          }
+  let node = parentEl.firstChild;
+  while (node) {
+    switch (flag) {
+      case SEARCHING_HEAD: {
+        if(node.nodeType === Node.TEXT_NODE && validHead.test(node.wholeText)) {
+          startNode = node;
+          title = getTitle(node.wholeText) || "展开 / 折叠";
+          flag = COLLECTING_DATA;
+        }
+        node = node.nextSibling;
+        break;
+      }
+      case COLLECTING_DATA: {
+        if(node.nodeType === Node.TEXT_NODE && validTail.test(node.wholeText)) {
+          endNode = node;
+          flag = ASSEMBLING_SPOILER;
+        }
+        else {
+          collapseNodes.push(node);
           node = node.nextSibling;
-          break;
         }
-        case COLLECTING_DATA: {
-          if(node.nodeType === Node.TEXT_NODE && validTail.test(node.wholeText)) {
-            endNode = node;
-            flag = ASSEMBLING_SPOILER;
-          }
-          else {
-            collapseNodes.push(node);
-            node = node.nextSibling;
-          }
-          break;
-        }
-        case ASSEMBLING_SPOILER: {
-          if(isBr(collapseNodes[0])) parentEl.removeChild(collapseNodes.shift());
-          if(isBr(collapseNodes[collapseNodes.length-1])) parentEl.removeChild(collapseNodes.pop());
+        break;
+      }
+      case ASSEMBLING_SPOILER: {
+        if(isBr(collapseNodes[0])) parentEl.removeChild(collapseNodes.shift());
+        if(isBr(collapseNodes[collapseNodes.length-1])) parentEl.removeChild(collapseNodes.pop());
 
-          const collapseWrapper = getCollapseEl(title, collapseNodes);
-          endNode.after(collapseWrapper);
+        const collapseWrapper = getCollapseEl(title, collapseNodes);
+        endNode.after(collapseWrapper);
 
-          node = node.nextSibling; //摆前面, 因为removeChild后就没有nextSibling了
-          parentEl.removeChild(startNode);
-          parentEl.removeChild(endNode);
-          reset();
-          break;
-        }
+        node = node.nextSibling; //摆前面, 因为removeChild后就没有nextSibling了
+        parentEl.removeChild(startNode);
+        parentEl.removeChild(endNode);
+        reset();
+        break;
       }
     }
   }
+}
 
-  //所有回复
-  document.querySelectorAll(".message, .cmt_sub_content").forEach(collapse);
-  if (location.pathname.includes("blog")) { //日志
-    document.querySelectorAll(".blog_entry").forEach(collapse);
-  } else if (location.pathname.includes("group") || location.pathname.includes("subject")) { //小组主题 & 条目讨论
-    document.querySelectorAll(".topic_content").forEach(collapse);
-  } else if (location.pathname.startsWith("/index")) { // 目录简介页
-    document.querySelectorAll("#columnSubjectBrowserA > div.grp_box > div > div > span").forEach(collapse);
-  }
-}) ();
+//所有回复
+document.querySelectorAll(".message, .cmt_sub_content").forEach(collapse);
+if (location.pathname.includes("blog")) { //日志
+  document.querySelectorAll(".blog_entry").forEach(collapse);
+} else if (location.pathname.includes("group") || location.pathname.includes("subject")) { //小组主题 & 条目讨论
+  document.querySelectorAll(".topic_content").forEach(collapse);
+} else if (location.pathname.startsWith("/index")) { // 目录简介页
+  document.querySelectorAll("#columnSubjectBrowserA > div.grp_box > div > div > span").forEach(collapse);
+}
