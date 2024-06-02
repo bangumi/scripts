5f17ae2c607036125121e47c8d1d07d280ab5113
XDcedar <402289412@qq.com>
2024-06-02 23:08:46 +0800

    改用detail和summary元素实现，代码重构

--- cedar/codeblock_super_evolution.user.js
+++ cedar/codeblock_super_evolution.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        代码块超进化！
 // @namespace   tv.bgm.cedar.codeblockSuperEvolution!
-// @version     2.0
+// @version     3.0
 // @description 进化！超代码块
 // @author      Cedar
 // @include     /^https?://((bangumi|bgm)\.tv|chii\.in)/.*$/
@@ -11,10 +11,10 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-(function () {
-  'use strict';
+'use strict';
 
-  GM_addStyle(`
+GM_addStyle(`
+/* for settings page */
 #code-block-settings {
   margin-top: 20px;
   font-size: 16px;
@@ -48,115 +48,242 @@
   border-right: none;
   transform: rotate(-45deg);
 }
+
+/* for code collapse */
+.code-block-collapse-wrapper {
+  padding: 10px;
+  border: 2px solid #EEE;
+  background-color: #FEFBFC;
+  border-radius: 5px;
+}
+
+html[data-theme='dark'] .code-block-collapse-wrapper {
+  border: 2px solid #444;
+  background-color: #302E30;
+}
+
+.code-block-collapse-wrapper pre {
+  padding-top: 10px;
+  border-top: 2px solid #EEE;
+  margin-top: 10px;
+}
+
+html[data-theme='dark'] .code-block-collapse-wrapper pre {
+  border-top: 2px solid #444;
+}
 `);
 
-  let code = document.querySelectorAll(".codeHighlight>pre");
-  let fontSettings = JSON.parse(localStorage['bgmcodeblockfont'] || "{}");
-  let userfontfamily = fontSettings.fontfamily || "";
-  let userfontsize = fontSettings.fontsize || "";
+function loadSettings() {
+  return JSON.parse(localStorage['bgmcodeblockfont'] || `{"fontfamily": "","fontsize": ""}`);
+}
 
-  const validCollapseCode = h => (h.startsWith('!===') && h.endsWith('===!') || h.startsWith('===') && h.endsWith('==='));
-  const keywords = ['font-style', 'font-variant', 'font-weight', 'font-size', 'line-height', 'font-family'];
+function saveSettings(fontfamily, fontsize) {
+  fontSettings.fontfamily = fontfamily;
+  fontSettings.fontsize = fontsize;
+  localStorage['bgmcodeblockfont'] = JSON.stringify(fontSettings);
+  $(".code-block-settings-save+span").fadeIn("fast").fadeTo(300, 1).fadeOut("slow");
+}
 
-  if (code.length) {
-    let defaultfontfamily = $(code[0]).css('font-family');
+const fontSettings = loadSettings();
+let defaultfontfamily = null;
 
-    //const collapseBtn = $(document.createElement('button'))
-    const collapseBtn = $(document.createElement('a'))
-      .addClass("chiiBtn")
-      .attr('href', 'javascript:;')
-      .css('margin', '5px')
-      .text("展开/折叠")
-      .on("click", function() {
-        $(this.nextElementSibling).fadeToggle("fast");
-      });
-    const codeTitle = $(document.createElement('span')).css('font-weight', 'bold');
+function removeRedundantBR(preNode) {
+  preNode.innerHTML = preNode.innerHTML.replace(/<br>\n/g, "<br>");
+}
+
+function validCollapseHead(head) {
+  return head.startsWith('===') && head.endsWith('===');
+}
+
+function validOpenHead(head) {
+  return head.startsWith('!===') && head.endsWith('===!');
+}
+
+function validCollapseCode(head) {
+  return validCollapseHead(head) || validOpenHead(head);
+}
+
+function noTitle(head) {
+  return /^!?=+!?$/.test(head); // 会允许类似 "!===" 这样的非法写法，要保证传入的参数是 valid 的
+}
+
+function validFontStyleLine(line) {
+  return line.startsWith("font") || line.startsWith("line-height");
+}
+
+function tryMakeCollapseCode(preNode) {
+  let head = preNode.innerHTML.split("<br>", 1)[0]; // 得到 "=== title ==="
+  if (!validCollapseCode(head))
+    return;
+
+  let title = parseTitle(head) || "展开 / 折叠";
+  const collapseWrapper = getCollapseEl(title);
+  preNode.insertAdjacentElement('beforebegin', collapseWrapper);
+  collapseWrapper.append(preNode);
+
+  // 注意处理没有<br>的单行代码块：[code]===[/code]
+  let i = preNode.innerHTML.indexOf("<br>");
+  if (i != -1)
+    preNode.innerHTML = preNode.innerHTML.slice(i + 4); // 去掉开头的 === title ===<br>
+
+  if (validOpenHead(head))
+    collapseWrapper.setAttribute("open", "");
+}
+
+function parseTitle(line) {
+  if (noTitle(line)) {
+    return "";
+  } else if (validCollapseHead(line)) {
+    return line.replace(/^=+\s*(.*?)\s*=+$/, "$1");
+  } else if (validOpenHead(line)) {
+    return line.replace(/^!=+\s*(.*?)\s*=+!$/, "$1");
+  } else {
+    return null;
+  }
+}
+
+// 用于组装折叠元素
+function getCollapseEl(title) {
+  const summaryEl = document.createElement('summary');
+  summaryEl.style.fontWeight = "bold";
+  summaryEl.style.cursor = "pointer";
+  summaryEl.innerText = title;
+  const collapseWrapper = document.createElement("details");
+  collapseWrapper.classList.add("code-block-collapse-wrapper");
+  collapseWrapper.append(summaryEl);
+
+  return collapseWrapper;
+}
+
+function tryAddLocalFontStyle(preNode) {
+  // 注意调用该函数前必须先把标题处理好
+  let head = preNode.innerHTML.split("<br>", 1)[0];
+  let style = parseLocalFontStyle(head);
+  if (style) {
+    Object.assign(preNode.style, style);
+  }
+}
+
+const fontStyleKeywords = ['font-style', 'font-variant', 'font-weight', 'font-size', 'line-height', 'font-family'];
+
+function parseLocalFontStyle(line) {
+  if (!validFontStyleLine(line))
+    return null;
+
+  // 用临时元素判定用户的 font style 有效性，并保存合法结果，更安全
+  let testEl = document.createElement('span');
+  testEl.setAttribute('style', line);
+  let fontStyles = {};
+  for (const k of fontStyleKeywords) {
+    let style = testEl.style[k];
+    if (style) fontStyles[k] = style;
+  }
+  if ('font-family' in fontStyles && fontSettings.fontfamily)
+    fontStyles['font-family'] += `, ${fontSettings.fontfamily}, ${defaultfontfamily}`;
+  return fontStyles;
+}
+
+function handleCodeBlock(preNodes) {
+  for (const c of preNodes) {
+    removeRedundantBR(c);
+    tryMakeCollapseCode(c);
+    tryAddLocalFontStyle(c);
+  }
+}
+
+function addUserFontStyleNode() {
+  if (fontSettings.fontfamily || fontSettings.fontsize) {
+    const style = document.createElement('style');
+    style.type = "text/css";
+    style.innerHTML = ".codeHighlight pre {"
+      + (fontSettings.fontfamily && `font-family: ${fontSettings.fontfamily}, ${defaultfontfamily};`)
+      + (fontSettings.fontsize && `font-size: ${fontSettings.fontsize}px;`)
+      + "}";
+    document.head.appendChild(style);
+  }
+}
+
+function addSettingsPage() {
+  // 尽可能不用 jQuery 实现
+  // let $settings = $(document.createElement('div')).attr('id', 'code-block-settings');
+  // let input = document.createElement('input'); input.type = "text"; input.maxLength = 100;
+  // let $fontFamily = $(input).clone().val(fontSettings.fontfamily).css('width', '120px');
+  // let $fontSize = $(input).clone().val(fontSettings.fontsize).css('width', '20px');
+
+  // $settings.append($(document.createElement('h2')).addClass('subtitle').text("Code块自定义"))
+  //   .append($(document.createElement('div')).append(
+  //     $(document.createElement('span')).text('font-family:'), $fontFamily))
+  //   .append($(document.createElement('div')).append(
+  //     $(document.createElement('span')).text('font-size:'), $fontSize, $(document.createElement('span')).text('px'),
+  //     $(document.createElement('a')).addClass("code-block-settings-save chiiBtn").text("保存").on("click", saveSettings),
+  //     $(document.createElement('span')).hide()));
+  // $('#columnB').append($settings);
+
+  // Create the settings element
+  const settings = document.createElement('div');
+  settings.id = 'code-block-settings';
+
+  // Create the title (h2)
+  const title = document.createElement('h2');
+  title.classList.add('subtitle');
+  title.textContent = "Code块自定义";
+
+  // Create the font-family label
+  const fontFamilyLabel = document.createElement('span');
+  fontFamilyLabel.textContent = 'font-family:';
+  // Create the input element for font-family
+  const fontFamilyInput = document.createElement("input");
+  fontFamilyInput.type = "text";
+  fontFamilyInput.maxLength = 100;
+  fontFamilyInput.value = fontSettings.fontfamily;
+  fontFamilyInput.style.width = "120px";
+  // Create font-family container and append input and label
+  const fontFamilyContainer = document.createElement('div');
+  fontFamilyContainer.append(fontFamilyLabel, fontFamilyInput);
+
+  // Create the input element for font-size
+  const fontSizeInput = fontFamilyInput.cloneNode();
+  fontSizeInput.value = fontSettings.fontsize;
+  fontSizeInput.style.width = "20px";
+  // Create the font-size label
+  const fontSizeLabel = document.createElement('span');
+  fontSizeLabel.textContent = 'font-size:';
+  // Create the font-size unit (px)
+  const fontSizeUnit = document.createElement('span');
+  fontSizeUnit.textContent = 'px';
+  // Create font-family container and append input label and unit
+  const fontSizeContainer = document.createElement('div');
+  fontSizeContainer.append(fontSizeLabel, fontSizeInput, fontSizeUnit);
+
+  // Create the save button and add click event listener
+  const saveButton = document.createElement('a');
+  saveButton.classList.add("code-block-settings-save", "chiiBtn");
+  saveButton.textContent = "保存";
+  saveButton.addEventListener('click', () => { saveSettings(fontFamilyInput.value, fontSizeInput.value) });
+
+  const savedHintEl = document.createElement('span');
+  savedHintEl.style.display = 'none';
+
+  // Append the settings element to the target container
+  settings.append(fontFamilyContainer, fontSizeContainer, saveButton, savedHintEl);
+  document.getElementById('columnB').appendChild(settings);
+}
+
+function main() {
+  const preNodes = document.querySelectorAll(".codeHighlight > pre");
+  if (preNodes.length) {
+    defaultfontfamily = window.getComputedStyle(preNodes[0]).getPropertyValue('font-family');
 
     //remove block and handle code head
-    for (let i = 0; i < code.length; i++) {
-      code[i].innerHTML = code[i].innerHTML.replace(/<br>\n/g, "<br>");
-      tryMakeCollapseCode(code[i]);
-      tryAddLocalFontStyle(code[i]);
-    }
+    handleCodeBlock(preNodes);
 
     //add user-defined font style
-    if (userfontfamily || userfontsize) {
-      let style = document.createElement('style'); style.type = "text/css";
-      style.innerHTML = ".codeHighlight>pre {"
-        + (userfontfamily? "font-family:"+userfontfamily+","+defaultfontfamily+";": "")
-        + (userfontsize? "font-size:"+userfontsize+"px;": "")
-        + "}";
-      document.head.appendChild(style);
-    }
-
-    //utility functions
-    function tryMakeCollapseCode(codeblock) {
-      let head = codeblock.innerHTML.split("<br>", 1)[0];
-      if (!validCollapseCode(head))
-        return;
-
-      let title = parseTitle(head) || "折叠区域";
-      $(codeblock).before(codeTitle.clone().text(title), collapseBtn.clone(true));
-
-      //注意处理没有<br>的代码块: [code]===[/code]
-      let i = codeblock.innerHTML.indexOf("<br>");
-      if (i != -1)
-        codeblock.innerHTML = codeblock.innerHTML.slice(i+4);
-
-      if (head.startsWith('=')) //Note: code validity has been confirmed
-        $(codeblock).hide();
-    }
-
-    function tryAddLocalFontStyle(codeblock) {
-      let head = codeblock.innerHTML.split("<br>", 1)[0];
-      let style = parseLocalFontStyle(head);
-      if (style) $(codeblock).css(style);
-    }
-
-    function parseTitle(line) {
-      if (line.startsWith('===') && line.endsWith('==='))
-        return line.replace(/^=+$/, "").replace(/^=+\s*(.*?)\s*=+$/, "$1");
-      if (line.startsWith('!===') && line.endsWith('===!'))
-        return line.replace(/^!=+!$/, "").replace(/^!=+\s*(.*?)\s*=+!$/, "$1");
-    }
-
-    function parseLocalFontStyle(line) {
-      if (!line.startsWith("font") && !line.startsWith("line-height"))
-        return null;
-
-      let testEl = document.createElement('span');
-      testEl.setAttribute('style', line);
-      let fontStyles = {};
-      for (let k of keywords) {
-        let style = testEl.style[k];
-        if (style) fontStyles[k] = style;
-      }
-      if ('font-family' in fontStyles)
-        fontStyles['font-family'] += ","+ (userfontfamily? userfontfamily+",": "")+defaultfontfamily;
-      return fontStyles;
-    }
+    addUserFontStyleNode();
   }
 
-  if (location.pathname=="/settings") {
-    let $settings = $(document.createElement('div')).attr('id', 'code-block-settings');
-    let input = document.createElement('input'); input.type = "text"; input.maxLength = 100;
-    let $fontFamily = $(input).clone().val(userfontfamily).css('width', '120px');
-    let $fontSize = $(input).clone().val(userfontsize).css('width', '20px');
-
-    $settings.append($(document.createElement('h2')).addClass('subtitle').text("Code块自定义"))
-      .append( $(document.createElement('div')).append(
-          $(document.createElement('span')).text('font-family:'), $fontFamily) )
-      .append( $(document.createElement('div')).append(
-          $(document.createElement('span')).text('font-size:'), $fontSize, $(document.createElement('span')).text('px'),
-          $(document.createElement('a')).addClass("code-block-settings-save chiiBtn").text("保存").on("click", saveSettings),
-          $(document.createElement('span')).hide()) );
-    $('#columnB').append($settings);
-
-    function saveSettings() {
-      fontSettings.fontfamily = $fontFamily.val();
-      fontSettings.fontsize = $fontSize.val();
-      localStorage['bgmcodeblockfont'] = JSON.stringify(fontSettings);
-      $(".code-block-settings-save+span").fadeIn("fast").fadeTo(300, 1).fadeOut("slow");
-    }
+  if (location.pathname == "/settings") {
+    addSettingsPage();
   }
-}) ();
+}
+
+main();
