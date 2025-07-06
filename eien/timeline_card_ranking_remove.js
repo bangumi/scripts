73fe0ef60dc9ad380a6b926bd2fdcba9e1bd631a
Zhuo Yue <zhuo0000yue@gmail.com>
2025-07-06 18:48:34 +0800

    修复一个移除评分失效的bug

--- eien/timeline_card_ranking_remove.js
+++ eien/timeline_card_ranking_remove.js
@@ -1,28 +1,36 @@
 // ==UserScript==
 // @name         移除动态评分
-// @namespace    https://bgm.tv/group/topic/388766
-// @version      0.1
-// @description  移除所有 .rateInfo元素
+// @namespace    http://tampermonkey.net/
+// @version      0.1.1
+// @description  移除 card 中的 .rateInfo元素
 // @author       默沨
-// @match        https://bangumi.tv/*
-// @match        https://bgm.tv/*
-// @match        https://chii.in/*
+// @match        https://bangumi.tv/
+// @match        https://bgm.tv/
+// @match        https://chii.in/
+// @match        https://bangumi.tv/user/*/timeline
+// @match        https://bgm.tv/user/*/timeline
+// @match        https://chii.in/user/*/timeline
 // @grant        none
 // ==/UserScript==
 
 (function () {
   "use strict";
-
   function removeRateInfo() {
-    let cards = document.querySelectorAll(".card");
-    cards.forEach(function (card) {
-      rateInfo.parentNode.removeChild(rateInfo);
+    document.querySelectorAll('.card .rateInfo').forEach(rateInfo => {
+      rateInfo.remove();
     });
   }
 
-  var observer = new MutationObserver(function (mutations) {
-    removeRateInfo();
-  });
-
+  function debounce(func, delay) {
+    let timer;
+    return function() {
+      clearTimeout(timer);
+      timer = setTimeout(() => func.apply(this, arguments), delay);
+    };
+  }
+  const debouncedRemove = debounce(removeRateInfo, 100);
+  var observer = new MutationObserver(debouncedRemove);
   observer.observe(document.body, { childList: true, subtree: true });
+
+  removeRateInfo();
 })();
