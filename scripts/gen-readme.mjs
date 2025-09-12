1ce9e965144fdcbeff1979b8a23aca80be1a6c42
inchei <inchei@protonmail.com>
2025-09-12 22:23:22 +0800

    添加版本检测

--- scripts/gen-readme.mjs
+++ scripts/gen-readme.mjs
@@ -6,7 +6,7 @@ import { exec } from 'child_process';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(path.dirname(__filename));
 
 const openFile = (filePath) => {
     let command;
