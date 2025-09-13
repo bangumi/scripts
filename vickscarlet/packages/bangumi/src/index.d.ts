cc105cae36a6d1f1d477fbfee91836ce4dc10b7f
Vick Scarlet <scarlet_vick@outlook.com>
2025-09-14 01:33:08 +0800

    add: funny script 打上贴贴

--- vickscarlet/packages/bangumi/src/index.d.ts
+++ vickscarlet/packages/bangumi/src/index.d.ts
@@ -1,6 +1,20 @@
+interface GeneralConfigParams {
+    title: string
+    name: string
+    type: string
+    defaultValue: string
+    getCurrentValue: () => void
+    onChange: (value: string) => void
+    options: { value: string; label: string }[]
+}
+
 var chiiLib: {
     ukagaka: {
         toggleTheme(): void
         toggleDisplay(): void
+        addGeneralConfig(params: GeneralConfigParams): void
+        removeGeneralConfig(params: any): void
+        addPanelTab(params: any): void
+        removePanelTab(params: any): void
     }
 }
