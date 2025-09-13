401cabb976f73c0b3ab404a8db0b9483f3c11edf
Vick Scarlet <scarlet_vick@outlook.com>
2025-09-13 17:10:50 +0800

    update: 优化 TextSvg

--- vickscarlet/packages/icon/src/TextSvg.tsx
+++ vickscarlet/packages/icon/src/TextSvg.tsx
@@ -1,31 +1,32 @@
-import { useState, useEffect } from 'react'
-function getWidth(text: string, size: number, className: string) {
-    const e = document.createElement('span')
-    document.body.append(e)
-    e.className = className
-    e.style.fontSize = `${size}px`
-    e.style.position = 'absolute'
-    e.style.opacity = '0'
-    e.append(document.createTextNode(text))
-    const w = e.offsetWidth
-    e.remove()
-    return w
-}
-
+import { useState, useEffect, useRef } from 'react'
 export interface Props {
     text: string
     className: string
 }
 
 export function TextSVG({ text, className }: Readonly<Props>) {
-    const [size] = useState(10)
+    const [height, setHeight] = useState(0)
     const [width, setWidth] = useState(0)
+    const textRef = useRef<SVGTextElement>(null)
     useEffect(() => {
-        setWidth(getWidth(text, size, className))
-    }, [size, text, className])
+        if (!textRef.current) return
+        const { width, height } = textRef.current.getBBox()
+        setWidth(width)
+        setHeight(height)
+    }, [text, className, textRef])
     return (
-        <svg className={className} fill="currentColor" viewBox={`0 0 ${width} ${size}`}>
-            <text fontSize={size}>{text}</text>
+        <svg className={className} fill="currentColor" viewBox={`0 0 ${width} ${height}`}>
+            <text
+                ref={textRef}
+                fontSize={10}
+                style={{
+                    transform: 'translate(50%,50%)',
+                    textAnchor: 'middle',
+                    dominantBaseline: 'central',
+                }}
+            >
+                {text}
+            </text>
         </svg>
     )
 }
