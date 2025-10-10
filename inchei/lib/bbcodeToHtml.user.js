657ce49c4fdfb251574df950bd50df6c880c2f8b
inchei <inchei@protonmail.com>
2025-10-10 20:35:07 +0800

    适配 bmo

--- inchei/lib/bbcodeToHtml.user.js
+++ inchei/lib/bbcodeToHtml.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bangumi BBCode to HTML
 // @namespace    bbcode.bangumi
-// @version      1.0.1
+// @version      1.1.0
 // @description  将 bangumi BBCode 转为 HTML
 // @author       you
 // @license      MIT
@@ -25,6 +25,9 @@
                                 ? `/img/smiles/tv_vs/bgm_${num}.png`
                                 : `/img/smiles/tv_500/bgm_${num}.${gifIds.has(num) ? 'gif' : 'png'}`;
                     return `<img src="${url}" smileid="${num + 16}" alt="(bgm${num})"${[124, 125].includes(num) ? ' width="21"' : ''}>`;
+                })
+                .replace(/\(bmo([A-Za-z0-9\-]+)\)/g, (_, code) => {
+                    return `<span class="bmo" data-code="(bmo${code})"><canvas class="bmoji-canvas" style="width: 21px; height: 21px;" width="63" height="63"></canvas></span>`;
                 })
                 .replace(/\[img(?:=(\d+),(\d+))?\]([^[]+)\[\/img\]/g, (_, w, h, url) =>
                     `<img class="code" src="${url.trim()}" rel="noreferrer" referrerpolicy="no-referrer" alt="${url.trim()}" loading="lazy"${w && h ? ` width="${w}" height="${h}"` : ''}>`
